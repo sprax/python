63d4a8d26c424e9fbaaec5e951ee99e1aaafeaab
Sprax Lines <sprax@comcast.net>
2016-06-13 16:59:52 -0400

    pylint

--- kadane.py
+++ kadane.py
@@ -1,8 +1,11 @@
+# kadane.py from the interwebs
+'''Kadane algorithm for finding the maximum sum of a contiguous subarray'''
 
-def max_contiguous_sum(A):
+def max_contiguous_sum(array):
+    '''function to find the maximum sum of a contiguous subarray'''
     max_so_far = max_ending_here = 0
-    for x in A:
-        max_ending_here = max(0, max_ending_here + x)
+    for elt in array:
+        max_ending_here = max(0, max_ending_here + elt)
         max_so_far = max(max_so_far, max_ending_here)
     return max_so_far
 
--- string2words.py
+++ string2words.py
@@ -1,124 +1,120 @@
-# python
-# Usage: python string2words.py words.txt rainrajatacozapsrakezarfabetrainzany
+#!/usr/bin/python3
+'''Parse a string into words
+Usage: python string2words.py words.txt rainrajatacozapsrakezarfabetrainzany
+'''
 
-# import modules
 import sys
  
 debug = 1
-WordCount = 0
 MinWordLength =  2
 MaxWordLength = 24
 hDictionary = {}
 
-
-def string2wordsFromEndOne(string, wordsAlreadyParsed):
+def string2wordsFromEndOne(string, words_already_parsed):
     '''recursively divide a string into string of words, backing up greedily from the end.
     Returns a string with spaces inserted between the words, or Node if the parse fails.'''
 
-    if (debug > 1):
-        print("string2wordFromEnds: string(", string, ") words(", wordsAlreadyParsed, ")\n")
+    if  debug > 1:
+        print("string2wordFromEnds: string(", string, ") words(", words_already_parsed, ")\n")
 
     # If the this string is a word, just return it with any words already parsed.
-    if isWord(string):
-        if wordsAlreadyParsed:
-            return string + " " + wordsAlreadyParsed
+    if is_word(string):
+        if words_already_parsed:
+            return string + " " + words_already_parsed
         else:
             return string
 
     # Else divide the string into two parts, and if the 2nd part is a word, keep going.
     # Use min and max word lengths to skip checking substrings that cannot be words.
-    maxIndex = len(string)
-    minIndex = maxIndex - MaxWordLength
-    if (minIndex < 0):
-        minIndex = 0
-    maxIndex -= MinWordLength;
-    while (maxIndex > minIndex):
-        substr = string[maxIndex:]
-        if isWord(substr):
-            if wordsAlreadyParsed:
-                substr += " " + wordsAlreadyParsed
-            moreWords = string2wordsFromEndOne(string[0:maxIndex], substr)
-            if moreWords:
-                return moreWords
-        maxIndex -= 1
+    max_index = len(string)
+    min_index = max_index - MaxWordLength
+    if  min_index < 0:
+        min_index = 0
+    max_index -= MinWordLength
+    while max_index > min_index:
+        substr = string[max_index:]
+        if is_word(substr):
+            if words_already_parsed:
+                substr += " " + words_already_parsed
+            more_words = string2wordsFromEndOne(string[0:max_index], substr)
+            if more_words:
+                return more_words
+        max_index -= 1
     return None          # string did not parse
 
 
-def string2wordsFromBegAll(string, wordsAlreadyParsed, allParses):
-    '''recursively divide a string into array of strings of words, going greedily from the beginning.
-    Returns a string with spaces inserted between the words, or None if the parse fails.'''
+def string2wordsFromBegAll(string, words_already_parsed, all_parses):
+    '''
+    Recursively divide a string into array of strings of words,
+    going greedily from the beginning.
+    Returns a string with spaces inserted between the words, or None if the parse fails.
+    '''
 
-    if (debug > 1):
-        print("string2wordsFromBegAll: string(", string, ") words(", wordsAlreadyParsed, ")\n")
+    if  debug > 1:
+        print("string2wordsFromBegAll: string(", string, ") words(", words_already_parsed, ")\n")
 
     # If the this string is a word, just return it with any words already parsed.
-    if isWord(string):
-        if wordsAlreadyParsed:
-            parsed = string + " " + wordsAlreadyParsed
+    if is_word(string):
+        if words_already_parsed:
+            parsed = string + " " + words_already_parsed
         else:
             parsed = string
-        allParses.append(parsed)
+        all_parses.append(parsed)
 
     # Else divide the string into two parts, and if the 2nd part is a word, keep going.
     # Use min and max word lengths to skip checking substrings that cannot be words.
-    maxIndex = len(string)
-    minIndex = maxIndex - MaxWordLength
-    if (minIndex < 0):
-        minIndex = 0
-    maxIndex -= MinWordLength;
-    while (maxIndex > minIndex):
-        substr = string[maxIndex:]
-        if isWord(substr):
-            if wordsAlreadyParsed:
-                substr += " " + wordsAlreadyParsed
-            string2wordsFromBegAll(string[0:maxIndex], substr, allParses)
-        maxIndex -= 1
+    max_index = len(string)
+    min_index = max_index - MaxWordLength
+    if  min_index < 0:
+        min_index = 0
+    max_index -= MinWordLength
+    while max_index > min_index:
+        substr = string[max_index:]
+        if is_word(substr):
+            if words_already_parsed:
+                substr += " " + words_already_parsed
+            string2wordsFromBegAll(string[0:max_index], substr, all_parses)
+        max_index -= 1
 
-    # No return value; any results were appended to allParses.
+    # No return value; any results were appended to all_parses.
 
 
-def loadDictionary(fileName):
+def load_dictionary(file_name):
     global hDictionary
-    wordCount = 0
-    for line in open(fileName):  # opened in text-mode; all EOLs are converted to '\n'
+    word_count = 0
+    for line in open(file_name):  # opened in text-mode; all EOLs are converted to '\n'
         line = line.rstrip('\n')
         size = len(line)
-        wordCount += 1;
+        word_count += 1
         hDictionary[line] = size
-    print("Read ", wordCount , " words from dictionary file: ", fileName, "\n");
+    print("Read ", word_count, " words from dictionary file: ", file_name, "\n")
 
-def isWord(string):
+def is_word(string):
     global hDictionary
     return hDictionary.get(string)
 
 
-def isPalindrome(data):
-    halfLength = int(len(data) / 2);
-    for j in range(halfLength):
-        if (data[j] != data[-j-1]):
+def is_palindrome(data):
+    half_len = int(len(data) / 2)
+    for j in range(half_len):
+        if  data[j] != data[-j-1]:
             return False
     return True
 
 
-def isPalindrome_test(string):
-    print("(1 > 0)", (1 > 0));
-    print("(1 > 2)", (1 > 2));
-    if (0):
-        print('0 is True')
-    if (1):
-        print('1 is True')
-    print("string", string, "is a palindrome? ", isPalindrome(string));
-    tuple = (1, 2, 3, 2, 1);
-    print("tuple", tuple, "is a palindrome? ", isPalindrome(tuple));
-    list = [4, 5, 6, 7, 7, 6, 5, 4];
-    print("list", list, "is a palindrome? ", isPalindrome(list));
+def test_is_palindrome(string):
+    print("string", string, "is a palindrome? ", is_palindrome(string))
+    i_tuple = (1, 2, 3, 2, 1)
+    print("i_tuple", i_tuple, "is a palindrome? ", is_palindrome(i_tuple))
+    i_list = [4, 5, 6, 7, 7, 6, 5, 4]
+    print("i_list", i_list, "is a palindrome? ", is_palindrome(i_list))
 
 
-if __name__ == '__main__':
+def test_string2words():
     if len(sys.argv) > 1:
-        fileName = sys.argv[1]
+        file_name = sys.argv[1]
     else:
-        fileName = "words.txt"
+        file_name = "words.txt"
 
     if len(sys.argv) > 2:
         strings = [sys.argv[2:]]
@@ -126,22 +122,25 @@ if __name__ == '__main__':
         strings = ["minimumergold", "garbageatone", 'atone']
 
 
-    isPalindrome_test(strings[0])
+    test_is_palindrome(strings[0])
 
-    loadDictionary(fileName)    
+    load_dictionary(file_name)
 
     if len(sys.argv) > 3:
         word = sys.argv[3]
-        print("isWord(", word, ") returned ", isWord(word), "\n");
+        print("is_word(", word, ") returned ", is_word(word), "\n")
 
-    numStr = len(strings)
-    for j in range(numStr):
-        ret = string2wordsFromEndOne(strings[j], "")
-        numParses = 0
+    for string in strings:
+        ret = string2wordsFromEndOne(string, "")
+        num_parses = 0
         if ret != None:
-            numParses = 1
-        print("string2wordsFromEndOne got", numParses, "(", ret, ")")
-        allParses = [];
-        ret =  string2wordsFromBegAll(strings[j], "", allParses)
-        numParses = len(allParses)
-        print("string2wordsFromBegAll got" + str(numParses) + "(" + str(allParses) + ")")
+            num_parses = 1
+        print("string2wordsFromEndOne got", num_parses, "(", ret, ")")
+        all_parses = []
+        string2wordsFromBegAll(string, "", all_parses)
+        num_parses = len(all_parses)
+        print("string2wordsFromBegAll got" + str(num_parses) + "(" + str(all_parses) + ")")
+
+if __name__ == '__main__':
+    test_string2words()
+
