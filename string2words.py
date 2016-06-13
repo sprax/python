fcb703681f8f50542a5fbd5b945857aa9bccfcb9
Sprax Lines <sprax@comcast.net>
2016-06-13 17:42:06 -0400

    pylinted

--- string2words.py
+++ string2words.py
@@ -4,18 +4,21 @@ Usage: python string2words.py words.txt rainrajatacozapsrakezarfabetrainzany
 '''
 
 import sys
- 
-debug = 1
-MinWordLength =  2
-MaxWordLength = 24
-hDictionary = {}
 
-def string2wordsFromEndOne(string, words_already_parsed):
-    '''recursively divide a string into string of words, backing up greedily from the end.
-    Returns a string with spaces inserted between the words, or Node if the parse fails.'''
+VERBOSE = 1
+MIN_WORD_LEN = 2
+MAX_WORD_LEN = 24
+DICTIONARY = {}
 
-    if  debug > 1:
-        print("string2wordFromEnds: string(", string, ") words(", words_already_parsed, ")\n")
+def string2words_from_end_one(string, words_already_parsed):
+    '''
+    Recursively divide a string into string of words, backing up greedily from the end.
+    Returns a string with spaces inserted between the words, or Node if the parse fails.
+    '''
+
+    if  VERBOSE > 1:
+        print("string2wordFromEnds: string(", string, ") words("
+              , words_already_parsed, ")\n")
 
     # If the this string is a word, just return it with any words already parsed.
     if is_word(string):
@@ -27,31 +30,32 @@ def string2wordsFromEndOne(string, words_already_parsed):
     # Else divide the string into two parts, and if the 2nd part is a word, keep going.
     # Use min and max word lengths to skip checking substrings that cannot be words.
     max_index = len(string)
-    min_index = max_index - MaxWordLength
+    min_index = max_index - MAX_WORD_LEN
     if  min_index < 0:
         min_index = 0
-    max_index -= MinWordLength
+    max_index -= MIN_WORD_LEN
     while max_index > min_index:
         substr = string[max_index:]
         if is_word(substr):
             if words_already_parsed:
                 substr += " " + words_already_parsed
-            more_words = string2wordsFromEndOne(string[0:max_index], substr)
+            more_words = string2words_from_end_one(string[0:max_index], substr)
             if more_words:
                 return more_words
         max_index -= 1
     return None          # string did not parse
 
 
-def string2wordsFromBegAll(string, words_already_parsed, all_parses):
+def string2words_from_beg_all(string, words_already_parsed, all_parses):
     '''
     Recursively divide a string into array of strings of words,
     going greedily from the beginning.
     Returns a string with spaces inserted between the words, or None if the parse fails.
     '''
 
-    if  debug > 1:
-        print("string2wordsFromBegAll: string(", string, ") words(", words_already_parsed, ")\n")
+    if  VERBOSE > 1:
+        print("string2words_from_beg_all: string(", string, ") words("
+              , words_already_parsed, ")\n")
 
     # If the this string is a word, just return it with any words already parsed.
     if is_word(string):
@@ -64,37 +68,37 @@ def string2wordsFromBegAll(string, words_already_parsed, all_parses):
     # Else divide the string into two parts, and if the 2nd part is a word, keep going.
     # Use min and max word lengths to skip checking substrings that cannot be words.
     max_index = len(string)
-    min_index = max_index - MaxWordLength
+    min_index = max_index - MAX_WORD_LEN
     if  min_index < 0:
         min_index = 0
-    max_index -= MinWordLength
+    max_index -= MIN_WORD_LEN
     while max_index > min_index:
         substr = string[max_index:]
         if is_word(substr):
             if words_already_parsed:
                 substr += " " + words_already_parsed
-            string2wordsFromBegAll(string[0:max_index], substr, all_parses)
+            string2words_from_beg_all(string[0:max_index], substr, all_parses)
         max_index -= 1
-
     # No return value; any results were appended to all_parses.
 
 
 def load_dictionary(file_name):
-    global hDictionary
+    '''load dictionary from text file, one word per line'''
     word_count = 0
     for line in open(file_name):  # opened in text-mode; all EOLs are converted to '\n'
         line = line.rstrip('\n')
         size = len(line)
         word_count += 1
-        hDictionary[line] = size
+        DICTIONARY[line] = size
     print("Read ", word_count, " words from dictionary file: ", file_name, "\n")
 
 def is_word(string):
-    global hDictionary
-    return hDictionary.get(string)
+    '''true iff dictionary word'''
+    return DICTIONARY.get(string)
 
 
 def is_palindrome(data):
+    '''true iff palindrome'''
     half_len = int(len(data) / 2)
     for j in range(half_len):
         if  data[j] != data[-j-1]:
@@ -103,6 +107,7 @@ def is_palindrome(data):
 
 
 def test_is_palindrome(string):
+    '''test if argument is a palindrome'''
     print("string", string, "is a palindrome? ", is_palindrome(string))
     i_tuple = (1, 2, 3, 2, 1)
     print("i_tuple", i_tuple, "is a palindrome? ", is_palindrome(i_tuple))
@@ -111,6 +116,7 @@ def test_is_palindrome(string):
 
 
 def test_string2words():
+    '''test the string2words functions'''
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
     else:
@@ -131,15 +137,15 @@ def test_string2words():
         print("is_word(", word, ") returned ", is_word(word), "\n")
 
     for string in strings:
-        ret = string2wordsFromEndOne(string, "")
+        ret = string2words_from_end_one(string, "")
         num_parses = 0
         if ret != None:
             num_parses = 1
-        print("string2wordsFromEndOne got", num_parses, "(", ret, ")")
+        print("string2words_from_end_one got", num_parses, "(", ret, ")")
         all_parses = []
-        string2wordsFromBegAll(string, "", all_parses)
+        string2words_from_beg_all(string, "", all_parses)
         num_parses = len(all_parses)
-        print("string2wordsFromBegAll got" + str(num_parses) + "(" + str(all_parses) + ")")
+        print("string2words_from_beg_all got" + str(num_parses) + "(" + str(all_parses) + ")")
 
 if __name__ == '__main__':
     test_string2words()
