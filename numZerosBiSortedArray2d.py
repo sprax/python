a2df3b4cc86ee2e1aa9daf1d2c01b993f5516a41
Sprax Lines <sprax@comcast.net>
2016-06-27 00:19:25 -0600

    algo

--- numZerosBiSortedArray2d.py
+++ numZerosBiSortedArray2d.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
-'''Python 2.7 script to count the number of zeros in a bi-sorted 2d array.'''
+'''Python 2.7 script to count the number of zeros in a bi-sorted 2d array.
+Fast algorithm for NxN matrix: Use the fact that M[j,j] == 0 implies M[p,q] == 0
+for any p <= j and q <= j.  Do a biniary search on the diagonal elements to
+find min k s.t. M[k,k] == 1,  Add k*k to count of zeros.  Now do the same,
+recursively, for the sub-matrices directly right of and below this k x k sub-matrix.
+Terminate the recursion when the next sub-matrix has zero extent.
+'''
 
 import operator
 
 def countZeros(array, length):
+    '''naive counting, O(MN)'''
     return length - reduce(operator.add, array)
 
 def test_countZeros():
