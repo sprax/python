604bafa5e09589a85c62dfa5f0951befa616fbf1
Sprax Lines <sprax@comcast.net>
2016-07-10 01:24:19 -0400

    corrections

--- sum_nested_iter.py
+++ sum_nested_iter.py
@@ -4,6 +4,7 @@ Usage (to run unit tests): python sum_nested_iter.py
 '''
 
 import collections
+from collections import deque
 
 def is_iterable(vit):
     '''Is vit an iterable?  Or just a scalar value?
@@ -11,15 +12,17 @@ def is_iterable(vit):
     '''
     return isinstance(vit, collections.Iterable)
 
-def is_indexible(vit):
-    '''Not so idiomatic way of testing if vit is indexible.
+def is_list(vit):
+    '''Not so idiomatic way of testing if vit is a list.
     Returns True if vit is a list, False if vit is a tuple or string.
     '''
     return type(vit) is type([])
 
 
 def sum_nested_iter_rec(vitlist, tot):
-    '''Sum all values in a list or typle of nested iterables.'''
+    '''Sum all values in a list or typle of nested iterables.
+    Relatively efficient, because it only makes one recursive call to itself
+    per nested iterable, but its first argument must be a list, not a scalar.'''
     for vit in vitlist:
         if is_iterable(vit):
             tot = sum_nested_iter_rec(vit, tot)
@@ -27,17 +30,32 @@ def sum_nested_iter_rec(vitlist, tot):
             tot = tot + vit
     return tot
 
-def sum_nested_index_rec(vitlist, tot):
-    '''Sum all values in a list or typle of nested indexibles.'''
+def sum_nested_rec(vit):
+    ''' Sum all values in a scalar argument, list or typle of nested iterables.
+    Expect this function to be less efficient than sum_nested_iter_rec, because it
+    makes at least one recursive call for every leaf element.'''
+    tot = 0
+    if is_iterable(vit):
+        for val in vit:
+            tot = tot + sum_nested_rec(val)
+    else:
+            tot = tot + vit
+    return tot
+
+def sum_nested_list_rec(vitlist, tot):
+    '''Sum all values in a list of nested lists.'''
     for vit in vitlist:
-        if is_indexible(vit):
-            tot = sum_nested_index_rec(vit, tot)
+        if is_list(vit):
+            tot = sum_nested_list_rec(vit, tot)
         else:
             tot = tot + vit
     return tot
 
+
 def sum_nested_iter_stack(vit):
-    '''Sum all values in a list or typle of nested iterables using a stack'''
+    '''Sum all values in a list or typle of nested iterables using a stack (depth-first,
+    meaning that nested iterables are processed as found, before processing the reest of
+    the current iterable).'''
     tot = 0
     stack = [vit]
     while stack:
@@ -50,32 +68,65 @@ def sum_nested_iter_stack(vit):
     return tot
 
 
+def sum_nested_iter_deque(vit):
+    '''Sum all values in a list or typle of nested iterables using a queue (breadth-first,
+    meaning that more deeply nested lists or tuples are processed later).'''
+    tot = 0
+    queue = deque([vit])
+    while queue:
+        vit = queue.popleft()
+        if is_iterable(vit):
+            for item in vit:
+                queue.append(item)
+        else:
+            tot = tot + vit
+    return tot
+
 
 def test_sum_nested_iter():
-    '''Test sum_nested_iter_rec'''
+    '''Test sum_nested_iter functions on nested lists and/or tuples'''
     print("Nested lists:")
     lst = [1, [2, 3], [4, [5, 6], 7], [[8, 9]]]
     print(lst)
     tot = sum_nested_iter_rec(lst, 0)
-    print("iter sum:  ", tot)
-    tot = sum_nested_index_rec(lst, 0)
-    print("index sum: ", tot)
+    print("reclist sum:", tot)
+    tot = sum_nested_rec(lst)
+    print("rec val sum:", tot)
+    tot = sum_nested_list_rec(lst, 0)
+    print("index sum:  ", tot)
     tot = sum_nested_iter_stack(lst)
-    print("stack sum: ", tot)
+    print("stack sum:  ", tot)
+    tot = sum_nested_iter_deque(lst)
+    print("deque sum:  ", tot)
     print()
     print("Tuple of nested lists and tuples:")
     tpl = (1, [2, 3], (4, (5, 6), 7), [(8, 9)])
     print(tpl)
     tot = sum_nested_iter_rec(tpl, 0)
-    print("iter sum:  ", tot)
+    print("rec sum:    ", tot)
     try:
-        tot = sum_nested_index_rec(tpl, 0)
-        print("index sum: ", tot)
+        tot = sum_nested_list_rec(tpl, 0)
     except TypeError as ex:
-        print("index sum: TypeError (expected) because is_indexible is False for tuples:\n\t", ex)
+        print("index sum: TypeError (expected) because is_list is False for tuples:\n\t", ex)
     tot = sum_nested_iter_stack(tpl)
-    print("stack sum: ", tot)
+    print("stack sum:  ", tot)
+    tot = sum_nested_iter_deque(lst)
+    print("deque sum:  ", tot)
 
+def test_sum_nested_iter_on_scalar():
+    '''Test sum_nested_iter functions on scalar arguments'''
+    num = 6
+    print("Number:", num)
+    try:
+        tot = sum_nested_iter_rec(num, 0) 
+    except TypeError as ex:
+        print("Recursive functions assume an iterable argument, thus: ", ex)
+    tot = sum_nested_iter_stack(num)
+    print("stack sum, number argument and sum: ", num, tot)
+    string = "string"
+    # tot = sum_nested_iter_stack(string)
+    # print("stack sum, string argument and sum: ", string, tot)
 
 if __name__ == '__main__':
     test_sum_nested_iter()
+    test_sum_nested_iter_on_scalar()
