f43fdd8862aa244d4af0e6807552678254d8a807
Sprax Lines <sprax@comcast.net>
2016-07-20 17:52:26 -0400

    hacker weird

---
+++ weirdnum.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/py-if-else?h_r=next-challenge&h_v=zen
+# Without using if-else (elif)
+
+import sys
+
+N = int(input().strip())
+if N % 2 == 0:
+    if N < 6 or N > 20:
+        print('Not ', end='')
+print('Weird')
