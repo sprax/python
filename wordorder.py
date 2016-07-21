9805dfc7e47c53aaffc9906f8a4f6b2d24e46bff
Sprax Lines <sprax@comcast.net>
2016-07-20 22:20:44 -0400

    filter orig stream

--- wordorder.py
+++ wordorder.py
@@ -1,22 +1,33 @@
-
 #!/bin/python3
-# 
+# https://www.hackerrank.com/challenges/word-order 
+'''
+Input Format
+
+The first line contains the integer, . 
+The next  lines each contain a word.
+
+Output Format
+
+Output  lines. 
+On the first line, output the number of distinct words from the input. 
+On the second line, output the number of occurrences for each distinct
+word according to their appearance in the input.
+'''
 
 count = int(input().strip())
 words = []
-for j in range(count):
-    words.append(input().strip())
-    
 dd = {}
-for wd in words:
+for j in range(count):
+    wd = input().strip()
     ct = dd.get(wd)
     if ct:
         dd[wd] = ct+1
     else:
         dd[wd] = 1
+        words.append(wd)
 
 print(len(dd))
 for wd in words:
-    print(dd.get(wd), ' ', end='')
+    print(dd.get(wd), '', end='')
 print()
 
