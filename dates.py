43b6081d08967c73d30a51045b89be707356150a
Sprax Lines <sprax@talla.com>
2016-09-27 23:59:16 -0400

    pylint

--- dates.py
+++ dates.py
@@ -2,24 +2,19 @@
 # Sprax Lines       2016.09.26      Written for Python 3.5
 '''Output some dates.'''
 
-# import modules
-from collections import defaultdict
-import glob
-import os
-import re
-# from stat import S_ISREG, S_ISDIR, ST_MTIME, ST_MODE
-from stat import ST_MTIME
-import shutil
 import sys
 import time
 import datetime
 
-dayCodes = ['Mnd', 'Tsd', 'Wnd', 'Thd', 'Frd', 'Std', 'Snd']
+DAY_CODES = ['Mnd', 'Tsd', 'Wnd', 'Thd', 'Frd', 'Std', 'Snd']
 
-def printDates(beg_day, num_day):
+def print_dates(beg_day, num_day):
     '''Output dates'''
     date = datetime.datetime.now()
-    for day in range(beg_day, num_day):
+    date += datetime.timedelta(days=beg_day)
+    tstm = date.timetuple()
+    print(tstm)
+    for _ in range(num_day):
         # print("day: ", day)
         date += datetime.timedelta(days=1)
         tstm = date.timetuple()
@@ -31,13 +26,14 @@ def printDates(beg_day, num_day):
             locs += 'NH'
         else:
             locs += 'MIT'
-        code = dayCodes[tstm.tm_wday]
+        code = DAY_CODES[tstm.tm_wday]
         # print(tstm)
         # print("%s ==> %s %s\t%s" % (date, dstr, code, locs))
         print("%s %s\t%s" % (dstr, code, locs))
 
 
 def main():
+    '''get args and call print_dates'''
     argc = len(sys.argv)
     if argc > 3:
         print(sys.argv[0])
@@ -46,7 +42,7 @@ def main():
     # Get the paths to the files (relative or absolute)
     num_day = int(sys.argv[1]) if argc > 1 else 7
     beg_day = int(sys.argv[2]) if argc > 2 else 0
-    printDates(beg_day, num_day)
+    print_dates(beg_day, num_day)
 
 if __name__ == '__main__':
     main()
