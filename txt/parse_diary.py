86468adeebf9e436f8f86ce62a3ce65b384a6aeb
Sprax Lines <sprax@comcast.net>
2016-11-05 18:32:36 -0400

    diary

--- txt/parse_diary.py
+++ txt/parse_diary.py
@@ -12,32 +12,6 @@ from utf_print import utf_print
 
 # DAY_CODES = ['Mnd', 'Tsd', 'Wnd', 'Thd', 'Frd', 'Std', 'Snd']
 
-def print_dates(out_format, start_date, offset_days, num_days, per_day, verbose):
-    '''Output num_days consecutive formatted dates from start_date'''
-    date = start_date
-    date += datetime.timedelta(days=offset_days)
-    # for _ in itertools.repeat(None, num_days):
-    for _ in range(num_days):
-        tstm = date.timetuple()
-        if verbose > 2:
-            print(tstm)
-        dstr = time.strftime(out_format, tstm)
-        if tstm.tm_wday < 5:
-            locs = 'Home/CIC'
-        elif tstm.tm_wday == 5:
-            locs = 'Home/NH'
-        else:
-            locs = 'Home/MIT'
-        # code = DAY_CODES[tstm.tm_wday]
-        # print(tstm)
-        # print("%s ==> %s %s\t%s" % (date, dstr, code, locs))
-        if per_day > 1:
-            print("%s AM \t%s" % (dstr, locs))
-            print("%s PM \t%s" % (dstr, 'Home'))
-        else:
-            print("%s\t%s" % (dstr, locs))
-        date += datetime.timedelta(days=1)
-
 def try_parse_date(text, in_formats):
     '''Try to extract a date by matching the input date formats.'''
     date = None
@@ -157,22 +131,14 @@ def dated_entry_regex():
 def main():
     '''get args and call print_dates'''
     default_format_out = '%Y.%m.%d %a'
-    default_num_days = 7
     default_jrnl_input = "djs.txt"
     # default_start_date = start_date = datetime.datetime.now()
     parser = argparse.ArgumentParser(
         # usage='%(prog)s [options]',
         description="Read/write journal-entry style dates"
         )
-    parser.add_argument('offset_days', type=int, nargs='?', default=0,
-                        help='offset from start date (default: today) to first output date')
-    parser.add_argument('num_days', type=int, nargs='?', default=default_num_days,
-                        help="number of days' dates to output (default: {})"
-                        .format(default_num_days))
-    parser.add_argument('per_day', type=int, nargs='?', default=1,
-                        help='number of entries per day (default: 1)')
-    parser.add_argument('-jrnl_input', metavar='FILE', type=str,
-                        help='convert dated-entry text file to jrnl format (default: {})'
+    parser.add_argument('jrnl_input', metavar='DIARY', type=str,
+                        help='dated-entry text file to jrnl format (default: {})'
                         .format(default_jrnl_input))
     parser.add_argument('-out_format', metavar='FORMAT', type=str, default=default_format_out,
                         help='output date format (default: {})'
@@ -193,11 +159,7 @@ def main():
             print("WARNING: Failed to parse start date: {}; using default: {}"
                   .format(args.start_date, start_date))
 
-    if args.jrnl_input:
-        reformat_journal(args.jrnl_input, in_formats, out_format, args.verbose)
-    else:
-        print_dates(out_format, start_date, args.offset_days, args.num_days
-                    , args.per_day, args.verbose)
+    reformat_journal(args.jrnl_input, in_formats, out_format, args.verbose)
 
 if __name__ == '__main__':
     main()
