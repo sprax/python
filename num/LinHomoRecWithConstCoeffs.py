de9dd721e7bfdd3e3578f69446f94d5853f335c5
Sprax Lines <sprax@comcast.net>
2016-12-26 13:15:44 -0500

    test

--- num/LinHomoRecWithConstCoeffs.py
+++ num/LinHomoRecWithConstCoeffs.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# Sprax Lines       2016.12.26      Written with Python 3.5
+"""LHRWCC: Linear Homogenous Recurrence With Constant Coefficients.
+   Class and unit tests.
+   """
+
 import unittest
 import fibonaccis
 
@@ -5,36 +11,41 @@ class LinHomoRecWithConstCoeffs:
     """LHRWCC: Linear Homogenous Recurrence With Constant Coefficients"""
 
     def __init__(self, coeffs, inits):
+        '''constructor'''
         assert len(coeffs) == len(inits)
         self.order = len(coeffs)
         self.coeffs = coeffs
         self.inits = inits
 
-    def a_n_rec(self, n):
-        if (n < self.order):
-            return self.inits[n]
+    def a_n_recurse(self, idx):
+        '''recursively computes Nth term in the SHRWCC sequence'''
+        if idx < self.order:
+            return self.inits[idx]
         tot = 0
         for k in range(self.order):
-            tot += self.coeffs[k] * self.a_n_rec(n - k - 1)
+            tot += self.coeffs[k] * self.a_n_recurse(idx - k - 1)
         return tot
 
 
 
 class TestLinHomoRecWithConstCoef(unittest.TestCase):
+    '''unit tests'''
 
     def setUp(self):
-        self.intArray = [y for y in fibonaccis.fib_generate(20) ]
-        self.testVals = [-2, 0, 1, 2, 20, 21, 22, 8888]
+        self.fib_array = [y for y in fibonaccis.fib_generate(32)]
         print(str(LinHomoRecWithConstCoeffs.__doc__))
         print(str(self.id()), '\n')
 
     def test_fibs(self):
+        '''Test against fibonaccis'''
         print("test_fibs")
         fibs = LinHomoRecWithConstCoeffs([1, 1], [1, 1])
         print("fibs.order: {}".format(fibs.order))
-        for j in range (30):
-            print("fibs.a_n_rec({}) => {}".format(j, fibs.a_n_rec(j)))
+        for j in range(30):
+            a_j = fibs.a_n_recurse(j)
+            f_j = self.fib_array[j+1]
+            print("fibs.a_n_recurse({}) => {} =?= {}".format(j, a_j, f_j))
+            assert a_j == f_j
 
 if __name__ == '__main__':
     unittest.main()
-
