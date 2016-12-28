1e11f00b8afd4de38eeb4dfd0d91bab34f2c43f4
Tully Lines <sprax@comcast.net>
2016-12-28 13:00:53 -0500

    rename

--- num/prime_divide.py
+++ num/prime_divide.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3 
 '''Find primes by checking each number in a range for divisors.  Primes have no divisors but 1 and themselves.'''
 
-for n in range(2, 53):
+for n in range(2, 100):
     for x in range(2, n):
         if n % x == 0:
-            print(str(n) + ' equals ' + str(x) + ' * ' + str(n/x))
+            print(str(n) + ' equals ' + str(x) + ' * ' + str(n//x))
             break
     else:
         # loop fell through without finding a factor
