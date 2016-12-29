3f6f1bfbf580582a93774f7cbe962d635540f42d
Sprax Lines <sprax@comcast.net>
2016-12-29 17:06:03 -0500

    print if level <= XDV_VERBOSITY

--- txt/xdv.py
+++ txt/xdv.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
-'''Debugging output and tools.   Python 3.5+'''
+'''
+XDV  == eXchange (I/O) Depending on Verbosity.
+Basic debugging out for Python 3.5+
+'''
 
-XDV_VERBOSITY = 1
+XDV_VERBOSITY = None
+XDV_DEFAULT = 0
 
-def xdv(verbosity, *args, **kwargs):
-    '''Conditional output: eXpress Depending on Verbosity'''
-    if verbosity > XDV_VERBOSITY:
-        print(*args, **kwargs)
-
-def set_xdv_threshold(level):
+def set_xdv_verbosity(verbosity):
     '''Set the modul-global variable XDV_VERBOSITY'''
     global XDV_VERBOSITY
-    XDV_VERBOSITY = level
+    if XDV_VERBOSITY != verbosity:
+        print("Setting XDV_VERBOSITY = {}".format(verbosity))
+        XDV_VERBOSITY = verbosity
 
-def test_debug():
+def xdv(level, *args, **kwargs):
+    '''Conditional output: eXpress Depending on Verbosity'''
+    if XDV_VERBOSITY is None:
+        print("WARNING: XDV_VERBOSITY was None (in {}); setting it to {}".format(__name__, XDV_DEFAULT))
+        set_xdv_verbosity(0)
+    if level <= XDV_VERBOSITY:
+        print(*args, **kwargs)
+
+def try_xdv():
     '''test xdv'''
-    xdv(1, "hi from xdv", 1)
-    xdv(1,"greetings from xdv")
-    xdv(2, "howdy from xdv")
-    xdv(2, "hello from xdv", "\n", "goodbye from xdv", sep='')
-    xdv(2, "hey from xdv", "YO", "\n{}".format("goodbye from xdv"), sep='')
+    xdv(0, "0 -- hi from xdv", 1)
+    xdv(1, "1 -- greetings from xdv")
+    xdv(2, "2 -- howdy from xdv")
+    xdv(3, "3 -- hello from xdv", "\n", "goodbye from xdv", sep='')
+    xdv(4, "4 -- hey from xdv", "YO", "\n{}".format("goodbye from xdv"), sep='')
+
+def test_xdv():
+    try_xdv()
+    set_xdv_verbosity(2)
+    try_xdv()
+    set_xdv_verbosity(4)
+    try_xdv()
 
 if __name__ == '__main__':
-    test_debug()
+    test_xdv()
