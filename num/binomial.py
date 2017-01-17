a9aa9703d9916b3b8862dffc042b45c9e518e169
Sprax Lines <sprax@comcast.net>
2017-01-16 23:58:43 -0500

    more

--- num/binomial.py
+++ num/binomial.py
@@ -6,9 +6,13 @@ import operator as op
 
 def ncr(n, r):
     r = min(r, n-r)
-    if r == 0: return 1
+    if r == 0:
+        return 1
     numer = reduce(op.mul, xrange(n, n-r, -1))
     denom = reduce(op.mul, xrange(1, r+1))
     return numer//denom
 
+print("ncr(3, 2) => ", ncr(4, 2))
 print("ncr(4, 2) => ", ncr(4, 2))
+print("ncr(5, 3) => ", ncr(5, 3))
+print("ncr(6, 5) => ", ncr(6, 5))
