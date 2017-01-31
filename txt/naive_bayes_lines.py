6c2f36f81531e4d505fff769bd8dc24511152ee7
Sprax Lines <sprax@comcast.net>
2017-01-31 00:56:26 -0500

    scores

--- txt/naive_bayes_lines.py
+++ txt/naive_bayes_lines.py
@@ -15,11 +15,12 @@ from collections import defaultdict
 import nltk
 import text_ops
 import text_fio
+from utf_print import utf_print
 
 LINE_MAX = 15
 
 class TextFileWordFreqs:
-    '''Text file line contents and word frequencies'''
+    '''Text file line contents and word frequencies: more than we need for naive Bayes'''
 
     def __init__(self, file_spec, min_freq=0.1, max_freq=0.9, verbose=1, charset='utf8'):
         '''Initialize the text file word counter.'''
@@ -28,100 +29,83 @@ class TextFileWordFreqs:
         self._stopwords = set(nltk.corpus.stopwords.words('english') + list(string.punctuation))
         self._filtered = False
         self._input_words = 0
-        self._count_words = 0
         self._text_lines = []
-        self._line_word_lists = []
+        self._line_word_dicts = []
         self._verbose = verbose
         self._word_counts = defaultdict(int)
-        sum_len = 0
+        self._sum_word_len = 0
         for line in text_fio.read_lines(file_spec, charset):
             self._text_lines.append(line)
-            sum_len += self.count_words(line)
-        print(self._text_lines[ 0])
-        print(self._text_lines[ 1])
-        print(self._text_lines[-1])
+            self._sum_word_len += self.count_words(line)
+        self._counted_words = self._input_words
+        utf_print(self._text_lines[ 0])
+        utf_print(self._text_lines[ 1])
+        utf_print(self._text_lines[-1])
+        print()
 
     def count_words(self, line):
         '''Add a single paragraph containing one or more sentences.
         Return the count of sentences in text'''
         word_hash = {}
         word_list = nltk.word_tokenize(line.lower())
+        self._input_words += len(word_list)
         # word_list = nltk.word_tokenize(sentence.decode("utf8").lower())
+        sum_len = 0
         for word in word_list:
-            self._input_words += 1
+            sum_len += len(word)
             self._word_counts[word] += 1
-            word_hash[word] = 1 + (word_hash[word] if word in word_hash else 0)
-        self._line_word_lists.append(word_hash)
-        return len(line)
+            # word_hash[word] = 1 + word_hash[word] if word in word_hash else 1
+            if word in word_hash:
+                word_hash[word] += 1 
+            else:
+                word_hash[word] = 1
+        self._line_word_dicts.append(word_hash)
+        return sum_len
 
-    def sentence_count(self):
+    def line_count(self):
         return len(self._text_lines)
 
     def filter_words(self):
         '''apply thresholding and remove stop words if not already filtered'''
         if not self._filtered:
-            self._count_words = text_ops.filter_word_counts(self._word_counts, self._stopwords,
+            self._counted_words = text_ops.filter_word_counts(self._word_counts, self._stopwords,
                 self._min_freq, self._max_freq, self._verbose)
             self._filtered = True
 
-    def summarize_all_idx(self, summary_count):
-        '''summarize all stored text and return indices of ranked extracted sentences'''
+    def most_modal_idx(self, count):
+        '''Return indices of count lines closest to normal for the set, ranked by self-similar score'''
         self.filter_words()
-        sentence_count = len(self._text_lines)
-        words_per_sentence = self._count_words / sentence_count
+        line_count = len(self._text_lines)
+        words_per_line = self._counted_words / line_count
         ranking = defaultdict(int)
-        for idx, snt_words in enumerate(self._line_word_lists):
-            ranking[idx] = self._score_sentence(snt_words, words_per_sentence)
-        return rank_dict_by_value(summary_count, ranking)
+        for idx, line_dicts in enumerate(self._line_word_dicts):
+            ranking[idx] = self._score_line_dict(line_dicts, words_per_line)
+        return rank_dict_by_value(count, ranking)
 
-    def summarize_all_snt(self, summary_count):
-        '''summarize all stored text and return the extracted sentences sorted by index'''
-        sents_idx = self.summarize_all_idx(summary_count)
-        sents_idx.sort()
-        return [self._text_lines[j] for j in sents_idx]
-
-    def summarize_next_idx(self, text, summary_count, summary_percent):
-        '''summarize another chunk of text, based on previous chunks,
-        and return ranked indices'''
-        saved_sentence_count = len(self._text_lines)
-        added_sentence_count = self.add_text(text)
-        if added_sentence_count < 1:
-            return []
-        self.filter_words()
-        total_sentence_count = len(self._text_lines)
-        assert total_sentence_count == saved_sentence_count + added_sentence_count
-        words_per_sentence = self._count_words / total_sentence_count
-        ranking = defaultdict(int)
-        summary_count, _ = text_ops.resolve_count(summary_count, summary_percent
-                , added_sentence_count)
-        for idx in range(saved_sentence_count, total_sentence_count):
-            snt_words = self._line_word_lists[idx]
-            ranking[idx] = self._score_sentence(snt_words, words_per_sentence)
-        sents_idx = rank_dict_by_value(summary_count, ranking)
-        return sents_idx
-
-    def summarize_next_snt(self, text, summary_count, summary_percent):
-        '''summarize another chunk of text, based on previous chunks,
-        and return sentences sorted by index'''
-        sents_idx = self.summarize_next_idx(text, summary_count, summary_percent)
-        sents_idx.sort()
-        return [self._text_lines[j] for j in sents_idx]
-
-    def _score_sentence(self, snt_words, words_per_sentence):
+    def _score_line_dict(self, line_dicts, words_per_line):
         score = 0
-        words = 0
-        for word, count in snt_words.items():
-            words += count
+        for word, count in line_dicts.items():
             if word in self._word_counts:
-                score += self._word_counts[word]
-        return score * math.log(words_per_sentence*(1.0 + 1.0/len(snt_words)))
-        # return score
+                score += count * math.log((1 + self._word_counts[word])/self._counted_words)
+            else:
+                print("Warning: _score_line_dict called on non-corpus line")
+        # return score * math.log(words_per_line*(1.0 + 1.0/len(line_dicts)))
+        return score
+
+    def score_text_line(self, word_list):
+        score = 0
+        for word in word_list:
+            if word in self._word_counts:
+                score += math.log((1 + self._word_counts[word])/self._counted_words)
+            elif self._verbose > 5:
+                print("_score_text_line: uncounted word: ", word)
+        return score
 
 ###############################################################################
 
-def rank_dict_by_value(summary_count, ranking):
-    '''Return the highest ranked N sentences.'''
-    return heapq.nlargest(summary_count, ranking, key=ranking.get)
+def rank_dict_by_value(count, ranking):
+    '''Return the highest ranked N lines.'''
+    return heapq.nlargest(count, ranking, key=ranking.get)
 
 ###############################################################################
 
@@ -139,17 +123,20 @@ def print_ranked_idx(idx, sorted_idx):
 
 ###############################################################################
 
-def classify_lines(class_files, opt, charset='utf8'):
-    """Output a summary of a text file."""
-
-    classes = []
-
-    # input class files:
-    for file_spec in class_files:
-        classes.append(TextFileWordFreqs(file_spec, opt.min_freq, opt.max_freq, opt.verbose))
-
+def classify_lines(class_file_specs, opt, charset='utf8'):
     '''Text file line contents and word frequencies'''
 
+    # input class files:
+    classes = []
+    for file_spec in class_file_specs:
+        classes.append(TextFileWordFreqs(file_spec, opt.min_freq, opt.max_freq, opt.verbose))
+
+    text_line = "No Mandrill was harmed in the making of this software release."
+    word_list = nltk.word_tokenize(text_line.lower())
+    scores = []
+    for line_class in classes:
+        scores.append(line_class.score_text_line(word_list))
+    print("Scores:", scores)
     exit(0)
 
 
@@ -157,21 +144,21 @@ def classify_lines(class_files, opt, charset='utf8'):
     out_file = text_fio.open_out_file(opt.out_file, label='summary')
 
     # Create summarizer and initialize with text:
-    sentence_count = text_freqs.add_text(text)
+    line_count = text_freqs.add_text(text)
 
     max_words = opt.max_print_words
 
     # Announce output:
     print(file_spec, '====>', '<stdout>' if out_file == sys.stdout else opt.out_file)
-    sum_count, act_percent = text_ops.resolve_count(opt.sum_count, opt.sum_percent, sentence_count)
+    sum_count, act_percent = text_ops.resolve_count(opt.sum_count, opt.sum_percent, line_count)
     print("Keeping {} ({:.4} percent) of {} sentences."
-            .format(sum_count, act_percent, sentence_count))
+            .format(sum_count, act_percent, line_count))
     print('-------------------------------------------------------------------')
 
     # Summarize and show results:
     if opt.indices_only:
         sents_idx = text_freqs.summarize_all_idx(sum_count)
-        print("Sentence indices in [0, {})".format(sentence_count))
+        print("Sentence indices in [0, {})".format(line_count))
         print_ranked_idx(sents_idx, sorted(sents_idx))
     else:
         summary_sentences = text_freqs.summarize_all_snt(sum_count)
@@ -184,8 +171,8 @@ def classify_lines(class_files, opt, charset='utf8'):
         print('-------------------------------------------------------------------', file=out_file)
         if opt.indices_only:
             sents_idx = text_freqs.summarize_next_idx(text, sum_count, opt.sum_percent)
-            sents_idx = [x - sentence_count for x in sents_idx]
-            print("Sentence indices in [0, {})".format(sentence_count))
+            sents_idx = [x - line_count for x in sents_idx]
+            print("Sentence indices in [0, {})".format(line_count))
             print_ranked_idx(sents_idx, sorted(sents_idx))
         else:
             summary_sentences = text_freqs.summarize_next_snt(text, sum_count, opt.sum_percent)
@@ -203,8 +190,8 @@ def main():
     parser = argparse.ArgumentParser(
         # usage='%(prog)s [options]',
         description="Extractive text summarizer")
-    parser.add_argument('text_spec', type=str, nargs='?', default='corpus.txt',
-                        help='text file containing text to summarize')
+    parser.add_argument('text_specs', type=str, nargs='+', default='corpus.txt',
+                        help='text files containing lines representative of classes')
     parser.add_argument('-index', dest='indices_only', action='store_true',
                         help='output only the indices of summary sentences')
     parser.add_argument('-list_numbers', action='store_true',
@@ -236,7 +223,7 @@ def main():
         exit(0)
 
     # summary_file = getattr(args, 'out_file', None)
-    classify_lines([args.text_spec], args)
+    classify_lines(args.text_specs, args)
 
 if __name__ == '__main__':
     main()
