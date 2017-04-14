46b98efed4751bdf6a58f2da2373b6d272390cbe
Sprax Lines <sprax@comcast.net>
2017-04-13 23:51:35 -0400

    sequences

--- txt/edit_dist.py
+++ txt/edit_dist.py
@@ -1,6 +1,12 @@
 
 import editdistance
-strA = 'banana'
-strB = 'bahamas'
-print("editdistance.eval({}, {}) = {}".format(strA, strB, editdistance.eval(strA, strB)))
 
+
+def edit_dist(seqA, seqB):
+    dist = editdistance.eval(seqA, seqB)
+    print("editdistance.eval({}, {}) = {}".format(seqA, seqB, dist))
+
+edit_dist('banana', 'bandana')
+edit_dist('banana', 'bahamas')
+edit_dist(['one', 'two'], ['one', 'three'])
+edit_dist(['one', 'two', 'three'], ['one', 'eleven', 'three'])
