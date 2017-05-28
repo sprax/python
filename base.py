3e57c4c96245179f5d4589bcf3173cb4fa4d85e2
Sprax Lines <sprax@comcast.net>
2017-05-28 14:13:52 -0400

    base

---
+++ base.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Sprax Lines       2016.07.12      Written with Python 3.5
+'''
+inheritance and MRO
+'''
+
+
+class Base(object):
+    def __init__(self, name):
+        self.name = name
+
+    def hi(self):
+        print("Hi, I'm %s!" % self.name)
+
+class Derived(Base):
+    def __init__(self, name, epi):
+        super().__init__(name)
+        self.name += " " + epi
+
+    def hi(self):
+        print("Hello, I'm %s!" % self.name)
+
+class PrefixMixin(object):
+
+
+
+
+def main():
+    verbose = 1
+    base = Base("joe")
+    base.hi()
+    derv = Derived("jerry", "hall")
+    derv.hi()
+
+if __name__ == '__main__':
+    main()
