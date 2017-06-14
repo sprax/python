1fdbebdef89ad1ed10d460d5e64b558bec61bc35
Sprax Lines <sprax@comcast.net>
2017-06-13 23:51:12 -0400

    plan

--- emoji/emoji.py
+++ emoji/emoji.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: iso-8859-15 -*-
+'''
+Plan:
+    Words|phrases -> words >= [phonetic syllables reprs]
+    Emojis >= [phontic syllable reprs]
+    Text => <syllabic repr> => <emoji>
+'''
 
 import emoji
 import re
 
 aa = ['??', '??', '??', '??', '??', '??']
 
+
 def extract_emojis(str):
-  return ''.join(c for c in str if c in emoji.UNICODE_EMOJI)
+    return ''.join(c for c in str if c in emoji.UNICODE_EMOJI)
 
 
 a_str = '?? ?? me así, bla es se ?? ds ??????'
