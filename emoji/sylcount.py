a26c61f95f3aefa6dcd6466f5cfcb8258ed43dd2
Sprax Lines <sprax@comcast.net>
2017-06-24 18:58:09 -0400

    seps

--- emoji/sylcount.py
+++ emoji/sylcount.py
@@ -10,7 +10,6 @@ Plan:
 '''
 
 import argparse
-import json
 import random
 import re
 import string
@@ -20,112 +19,51 @@ import emoji
 import emotuples
 
 EXAMPLES = {
-    "There isn't one empyrean ouroborous; everyone knows there've always been several" : 24,
+    "There isn't one empyrean ouroborous; everyone knows there've always been several." : 24,
     #  1   2  3  4   5  6 78   9 0 1  2   3 4* 5     6     7  8   9  0    1    2 3 4
+    '"There aren\'t really any homogeneous cultures," you\'ll say, "they\'ve always shown rifts."' : 21,
+    #   1   2        3   5 6 7  8 9 0 1 2   3  4        5      6      7      8  9     0    1
     '"Ain\'t all y\'all young\'uns under 17," she\'d said, "like 11- or 15-years old?!"' : 21,
     #  1     2      3    4     5   6  7  +3     1     2      3   +3  7  +2  0    1}
+    "Didn't you know my X-15's XLR-99 engine burned 15,000 pounds (6,717 kg) of propellant in 87 seconds?" : 35,
+    # 1  2   3    4   5 6 +2    +3 +2 4  5    6     +2  +2  1     +3  +8 +3  6    7 8  9   0  +3  4 5
 }
 
 def qw(ss):
     return ss.split()
 
 VOWEL_GROUPS = qw('a ae ai ay e ea ei eu ey i ie iou o oa oi ou oy u')
-WORD_SEP_CHARACTERS = r'!"#$%&()*+,./:;<=>?@[\]^_`{|}~\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99'
-WORD_SEP_BOUNDARIES = r"'-"
 
-REGEX_WORD_SEP = r"(?:\s+['-]+\s*|\s*['-]+\s+|[\s{}]+|\s+)".format(WORD_SEP_CHARACTERS)
+###############################################################################
+WORD_SEP_INTERIOR = r"',.-"
+RE_WORD_TOKEN = re.compile(r"((?:\w+[{}]\w*)+\w|\w+)".format(WORD_SEP_INTERIOR))
 
+def word_tokens(sentence):
+    return RE_WORD_TOKEN.findall(sentence)
+
+###############################################################################
+WORD_SEP_EXTERIOR = r'!"#$%&()*+./:;<=>?@[\]^_`{|}~\x82\x83\x84\x85\x86\x87\x88\x89' \
+                    r'\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99'
+
+RE_WORD_SEP = re.compile(r"(?:\s+[{}]+\s*|\s*[{}]+\s+|[{}]*[\s{}]+|\s+)".format(
+    WORD_SEP_INTERIOR, WORD_SEP_INTERIOR, WORD_SEP_INTERIOR, WORD_SEP_EXTERIOR))
+
+def word_splits(sentence):
+    splits = RE_WORD_SEP.split(sentence)
+    return [ss for ss in splits if len(ss) > 0]
+
+###############################################################################
 REGEX_PUNCTUATION = re.compile("[{}]".format(string.punctuation))
 REGEX_NON_ALPHA = re.compile(r'(?:\W|[0-9])+')
 
+###############################################################################
 def qw_list(ss):
     return re.split(r'\W+', ss.rstrip())
 
 def qw_tuple(ss):
     return tuple(qw_list(ss))
 
-def test_misc():
-    trans()
-    print(u'\U0001f604'.encode('unicode-escape'))
-    print(u'\U0001f604')
-    ss = u'\U0001f604'
-    xx = chr(ss[0])
-    print("ss({}) xx({})".format(ss, xx))
-    # -*- coding: UTF-8 -*-
-    #convert to unicode
-    teststring =  "I am happy \U0001f604"
-    # #teststring = unicode(teststring, 'utf-8')
-
-    #encode it with string escape
-    teststring = teststring.encode('unicode_escape')
-    print("💗 Growing Heart")
-    print(emoji.emojize('Water! :water_wave:'))
-    print(emoji.demojize(u'🌊')) # for Python 2.x
-# print(emoji.demojize('🌊')) # for Python 3.x.
-    print(u"And \U0001F60D")
-    print("(-woman) astronaut", chr(int("0001f680", 16)))
-    print("woman_astronaut", chr(int("0x0001f680", 0)))
-
-    print("\U0001f483\U0001f3fe")
-
-    print(chr(0x001f483),chr(0x001f3fe))
-    print('💃 🏾 ')
-    print(chr(0x001f483)+chr(0x001f3fe))
-    print('💃🏾 ')
-    print(chr(int('1f483',16))+chr(int('1f3fe',16)))
-    print('%8s %8s %8s' % qw_tuple('surf wave whitecap'))
-    print("('%s', '%s', '%s')" % qw_tuple("surf's-up wave rip-curl"))
-
-
-def gen_emo_tuples(emo_dict, limit=5000):
-    for i, t in enumerate(sorted(jt.items(), key=lambda x: int(x[1]['order']))):
-        if i > limit:
-            break
-        key, val, chk = t[0], t[1], unicode_chr_str(key)
-        print("('%s', '%s', %d, %d, '%s', %s, '%s', [])," % (key, chk, int(val['order']), len(chk), val['shortname'], val['shortname_alternates'], val['category']))
-
-
-class EmoTuples:
-    def __init__(self):
-        self.emo_tuples = EMO_TUPLES
-
-
-
-def emojize(src_to_emo, txt_phrase):
-    srcs = re.split(r'\W+', txt_phrase.rstrip())
-    emo_phrase = []
-    for src in srcs:
-        lst = src_to_emo[src]
-        num = len(lst)
-        if num > 1:
-            dst = random.choice(lst)
-        elif num == 1:
-            dst = lst[0]
-        else:
-            dst = src
-        emo_phrase.append(dst)
-    return emo_phrase
-
-def test_emo_tuples(options):
-    presets = {}
-    if options.no_articles:
-        presets.update({'a': [''], 'but': [''], 'may': [''], 'the': ['']})
-    if options.subtraction:
-        presets.update({'can': ['🍬 ➖ D']})
-    src_to_emo = defaultdict(list, presets)
-    i_monosyllables = emotuples.INDEX_MONOSYLLABLES
-    i_unichr = emotuples.INDEX_EMOJI_UNICHRS
-    for tt in emotuples.EMO_TUPLES:
-        for src in tt[i_monosyllables]:
-            # print("type src is: ", type(src), "and uni:", tt[0])
-            src_to_emo[src].append(tt[i_unichr])
-            # print("src(%s) => emo(%s)" % (src, tt[1]))
-    txt_phrase = WAW
-    emo_list = emojize(src_to_emo, txt_phrase)
-    emo_phrase = ' '.join(emo_list)
-    print("    %s ==>\n    %s" % (txt_phrase, emo_phrase))
-
-def test_it():
+def main():
     '''test english -> emoji translation'''
     parser = argparse.ArgumentParser(
         # usage='%(prog)s [options]',
@@ -151,7 +89,16 @@ def test_it():
                         help='verbosity of output (default: 1)')
     args = parser.parse_args()
     # test_misc()
-    print('VOWEL_GROUPS:\n', VOWEL_GROUPS)
+    # print('VOWEL_GROUPS:', VOWEL_GROUPS, '\n')
+
+    for key, val in EXAMPLES.items():
+        print("MANUAL", val, key)
+        tokens = word_tokens(key)
+        print("TOKENS", len(tokens), tokens)
+        swords = word_splits(key)
+        print("SPLITS", len(swords), swords)
+        print()
+
 
 if __name__ == '__main__':
-    test_it()
+    main()
