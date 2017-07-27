eb72dedb0e8b429244e2939f8624191606a37f30
Sprax Lines <sprax@comcast.net>
2017-07-27 17:22:36 -0400

    matcher

---
+++ matcher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Sprax Lines       2016.07.25      Written with Python 3.5
+'''print not necessarily ASCII text file to terminal'''
+
+import json
+import re
+import sys
+
+
+def load_json_file(path, key='rules'):
+    with open(path) as fh:
+        return json.load(fh)[key]
+
+def match_rules(body):
+    for rule in rules:
+        match = rule['re'].match(body)
+        if match:
+            print("pattern({})  body({})  match({})  groups({})".format(rule['pattern'], body, match, match.groups()))
+
+def compile(rules):
+    """
+    Precompiles all pattern rules into an 're' key.
+    """
+    for rule in rules:
+        rule['re'] = re.compile(rule['pattern'], re.IGNORECASE)
+
+def load_rules(path='/Users/sprax/dev/magic/data/social_graces_regex.json'):
+    rules = load_json_file(path, 'rules')
+    compile(rules)
+    return rules
+
+
+
+
+
+
+def my_print(line):
+    print(line.encode("utf-8"))
+
+
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
