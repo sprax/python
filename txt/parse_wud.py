6cb642867d5ef863bba0701d55798cd0e482f10f
Sprax Lines <sprax@comcast.net>
2017-07-28 23:57:52 -0400

    is_blank_line

--- txt/parse_wud.py
+++ txt/parse_wud.py
@@ -26,58 +26,6 @@ RE_PARA_SEPARATOR = re.compile(r'\s*\n')
 
 ################################################################################
 
-def filter_stop_word_counts(word_counts, stopwords):
-    """ remove any word in stopwords or whose count is below the min or above the max threshold """
-    count_removed = 0
-    for word in stopwords:
-        count_removed += word_counts.pop(word, 0)
-    return count_removed
-
-################################################################################
-
-def filter_word_counts(word_counts, stopwords, min_freq, max_freq, verbose):
-    """ remove any word in stopwords or whose count is below the min or above the max threshold """
-    max_word_count = 0
-    for word, count in word_counts.items():
-        if count > max_word_count and word not in stopwords:
-            max_word_count = count
-    min_freq_count = max_word_count * min_freq
-    max_freq_count = max_word_count * max_freq
-    stop_words_to_remove = []
-    rare_words_to_remove = []
-    total_count = 0
-    for word, count in word_counts.items():
-        if count > max_freq_count or word in stopwords:
-            stop_words_to_remove.append(word)
-        elif count < min_freq_count:
-            rare_words_to_remove.append(word)
-        else:
-            total_count += count
-    if verbose > 2:
-        utf_print("========Removing common words: ", stop_words_to_remove)
-        for key in stop_words_to_remove:
-            word_counts.pop(key, None)
-        utf_print("========Removing rarest words: ", rare_words_to_remove)
-        for key in rare_words_to_remove:
-            word_counts.pop(key, None)
-    return total_count
-################################################################################
-
-def resolve_count(sub_count, percent, total_count):
-    '''returns reconciled sub-count and percentage of total, where count trumps percentage'''
-    if total_count < 1:
-        raise ValueError("total_count must be > 0")
-    if not sub_count:
-        sub_count = int(math.ceil(percent * total_count / 100.0))
-    if  sub_count > total_count:
-        sub_count = total_count
-    if  sub_count < 1:
-        sub_count = 1
-    percent = sub_count * 100.0 / total_count
-    return sub_count, percent
-
-################################################################################
-
 def paragraph_iter(fileobj, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0):
     r'''
     yields paragraphs from text file and regex separator, which by default matches
@@ -109,17 +57,20 @@ def paragraph_iter(fileobj, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0):
     if paragraph:
         yield paragraph
 
+def is_blank_line(line):
+    for char in line:
+        if char != ' ' and char != '\t' and char != '\n' and char != '\r':
+            return False
+    return True
 
-def lex_entry_iter(fileobj, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0):
+################################################################################
+def lex_entry_ns_iter(fileobj, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0):
     '''
-    yields paragraphs representing lexion entries from a text file and regex separator,
-    which by default matches a blank line followed by an all uppercase word.
-    All but the first
-    paragraph will begin with the separator, unless it contains only whitespace,
-    which is stripped from the end of every line.
-    TODO: Consider adding span-matching for square brackets, that is, never
-    breaking a paragraph on text between an open bracket [ and a matching, i.e.
-    balance end bracket ].  Likewise { no break between curly braces }.
+    Yields paragraphs (including newlines) representing lexion entries from a text
+    file and regex separator, which by default matches a blank line followed by an
+    all-uppercase word.  The all-caps word is included in the paragraph, as are any
+    newlines upto the next paragraph separator.  In general, lines are not stripped
+    of whitespace.
     '''
     ## Makes no assumptions about the encoding used in the file
     paragraph, blank_lines, entry_lines = '', 0, 0
@@ -128,8 +79,11 @@ def lex_entry_iter(fileobj, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0):
         if re.match(rgx_para_separator, line) and paragraph and blank_lines >= sep_lines:
             yield paragraph
             paragraph, blank_lines, entry_lines = '', 0, 0
-        line = line.rstrip()
-        if line:
+        if is_blank_line(line):
+            if entry_lines == 2:
+                paragraph += "\t"
+            blank_lines += 1
+        else:
             blank_lines = 0
             entry_lines += 1
             if not paragraph:
@@ -139,10 +93,6 @@ def lex_entry_iter(fileobj, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0):
                 paragraph += line               # hyphen, not suffix marker
             else:
                 paragraph += ' ' + line
-        else:
-            if entry_lines == 2:
-                paragraph += "\t"
-            blank_lines += 1
     if paragraph:
         yield paragraph
 
@@ -166,114 +116,6 @@ def paragraph_multiline_iter(fileobj, rgx_para_separator=r'\s*\n\s*\n\s*|\s*\n\t
     if paragraph:
         yield paragraph
 
-def print_paragraphs(path, charset='utf8', rgx_para_separator=RE_PARA_SEPARATOR):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs:")
-    with open(path, 'r', encoding=charset) as text:
-        for idx, para in enumerate(paragraph_iter(text, rgx_para_separator)):
-            print("    Paragraph {}:".format(idx))
-            utf_print(para)
-            print()
-
-def print_paragraphs_split_join_str(path, max_words, charset='utf8',
-                                    rgx_para_separator=RE_PARA_SEPARATOR):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs:")
-    with open(path, 'r', encoding=charset) as text:
-        for idx, para in enumerate(paragraph_iter(text, rgx_para_separator)):
-            words = para.split()[:max_words]
-            print("    Paragraph {}:".format(idx))
-            utf_print(' '.join(words))
-            print()
-
-def print_paragraphs_split_join_rgx(path, max_words, rgx_pat=r'^|\W*\s+\W*', charset='utf8'):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs:")
-    with open(path, 'r', encoding=charset) as text:
-        for idx, para in enumerate(paragraph_iter(text)):
-            words = re.split(rgx_pat, para)[:max_words]
-            print("    Paragraph {}:".format(idx))
-            utf_print(' '.join(words))
-            print()
-
-def print_paragraphs_nth_substr(path, max_words, charset='utf8'):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs_nth_substr(", path, max_words, charset, ")")
-    with open(path, 'r', encoding=charset) as text:
-        for idx, para in enumerate(paragraph_iter(text)):
-            index = index_substr_nth(para, max_words)
-            print("    Paragraph {}:".format(idx))
-            utf_print(para[:index])
-            print()
-
-def print_paragraphs_nth_regex(path, max_words=INF_NUM_WORDS, out_file=sys.stdout, charset='utf8'):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs_nth_substr(", path, max_words, charset, ")")
-    with open(path, 'r', encoding=charset) as text:
-        for para in paragraph_iter(text):
-            print_paragraph_regex_count(para, max_words, out_file)
-            print(file=out_file)
-
-def print_paragraph_regex_count(para, max_words=INF_NUM_WORDS, out_file=sys.stdout,
-                                elliptical='...'):
-    '''split paragraph into words using regex and print up to max_words words.'''
-    if para:
-        index = index_regex_count(para, max_words)
-        if elliptical and len(para) - index > 3:
-            utf_print(para[:index], '...', outfile=out_file)
-        else:
-            utf_print(para[:index], outfile=out_file)
-
-def index_regex_count(string, count=0, rgx=re.compile(r'\s+|$')):
-    '''Character index of Nth or last occurrence of regex pattern in string.
-    Returns the index of the Nth occurrence where N <= count, or
-    -1 if the pattern is not found at all.'''
-    offset = 0
-    if not string:
-        return offset
-    lens = len(string)
-    for _ in range(count):
-        if lens <= offset:
-            return offset
-        mat = rgx.search(string, offset)
-        if not mat:
-            return offset
-        mat_span = mat.span()
-        offset = mat_span[1]
-    return mat_span[0]
-
-def index_substr_nth(string, count=0, subs=' ', overlap=False):
-    '''index of nth occurrence of substring in string'''
-    skip = 1 if overlap else len(subs)
-    index = -skip
-    for _ in range(count + 1):
-        index = string.find(subs, index + skip)
-        if index < 0:
-            break
-    return index
-
-def index_regex_nth(string, count=0, rgx=re.compile(r'\s+|$')):
-    '''index of Nth occurrence of regex pattern in string, or -1 if count < n'''
-    offset = 0
-    for _ in range(count + 1):
-        mat = rgx.search(string, offset)
-        if not mat:
-            return -1
-        mat_span = mat.span()
-        offset = mat_span[1]
-    return mat_span[0]
-
-
-def print_paragraphs_trunc(path, max_words, charset='utf8'):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs:")
-    with open(path, 'r', encoding=charset) as text:
-        for idx, para in enumerate(paragraph_iter(text)):
-            words = para.split()[:max_words]
-            print("    Paragraph {}:".format(idx))
-            utf_print(' '.join(words))
-            print()
-
 def paragraph_reader(path, charset="utf8", rgx_para_separator=RE_PARA_SEPARATOR):
     '''opens text file and returns paragraph iterator'''
     try:
@@ -282,32 +124,6 @@ def paragraph_reader(path, charset="utf8", rgx_para_separator=RE_PARA_SEPARATOR)
     except FileNotFoundError as ex:
         print("Warning:", ex)
         return None, None
-
-def print_paragraphs_leaky(path):
-    '''Prints sequence numbers and paragraphs.'''
-    print("print_paragraphs:")
-    para_iter, fileobj = paragraph_reader(path)
-    for idx, para in enumerate(para_iter):
-        print("    Paragraph {}:".format(idx))
-        utf_print(para)
-        print()
-    fileobj.close()
-
-
-def print_sentences(sentences, list_numbers, max_words, out_file):
-    '''prints an array of sentences, with optional numbering'''
-    if list_numbers:
-        if max_words > 0 and max_words < 15:
-            idx_format = '{} '
-        else:
-            idx_format = '\n    {}\n'
-    for idx, sentence in enumerate(sentences):
-        if list_numbers:
-            print(idx_format.format(idx), end=' ')
-        if max_words:
-            print_paragraph_regex_count(sentence, max_words, out_file=out_file)
-        else:
-            utf_print(sentence, outfile=out_file)
 
 def para_iter_file(path, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0, charset='utf8'):
     '''Generator yielding filtered paragraphs from a text file'''
@@ -320,34 +136,12 @@ def para_iter_file(path, rgx_para_separator=RE_PARA_SEPARATOR, sep_lines=0, char
 REP_UPPER_WORD = r'^\s*([A-Z-]+)\b\s*'
 REC_UPPER_WORD = re.compile(REP_UPPER_WORD)
 
-def para_iter_lex_file(path, rgx_para_separator=REC_UPPER_WORD, sep_lines=0, charset='utf8'):
-    '''Generator yielding filtered paragraphs from a lexicon file'''
-    # print("para_iter_lex_file: pattern: %s" % rgx_para_separator.pattern)
+def para_ns_iter_lex_file(path, rgx_para_separator=REC_UPPER_WORD, sep_lines=0, charset='utf8'):
+    '''Generator yielding filtered paragraphs (including newlines) from a lexicon file'''
+    # print("para_ns_iter_lex_file: pattern: %s" % rgx_para_separator.pattern)
     with open(path, 'r', encoding=charset) as text:
-        for para in lex_entry_iter(text, rgx_para_separator, sep_lines):
+        for para in lex_entry_ns_iter(text, rgx_para_separator, sep_lines):
             yield para
-
-def filter_file(para_filter, path, charset='utf8'):
-    '''Generator yielding filtered paragraphs from a text file'''
-    with open(path, 'r', encoding=charset) as text:
-        for para in paragraph_iter(text):
-            yield para_filter.filter_paragraph(para)
-
-
-Webster = namedtuple('Webster',
-                     'word_1 word_2 pron_1 parenth bracket sep_spc part_1 etymology defn1 usage1 defn2 etc')
-
-def print_webster(webster):
-    '''print a Webster namedtuple'''
-    print("    Webster tuple:")
-    print("\tword:", webster.word_1)
-    print("\tpron:", webster.pron_1)
-    print("\tpart:", webster.part_1)
-    print("\tetym:", webster.etymology)
-    print("\tdef1:", webster.defn1)
-    print("\tuse1:", webster.usage1)
-    print("\tdef2:", webster.defn2)
-    print("\tmore:", webster.etc)
 
 def put(*args, sep=''):
     print(*args, sep=sep)
@@ -616,7 +410,7 @@ def parse_webster_file(path, opts, verbose=1):
             print("Partial____%s____" % partial[comlen-24:comlen+20])
         else:
             print("Partial == Webster pattern:", totlen)
-    for idx, entry_text in enumerate(para_iter_lex_file(path, REC_UPPER_WORD, sep_lines=1, charset=opts.charset)):
+    for idx, entry_text in enumerate(para_ns_iter_lex_file(path, REC_UPPER_WORD, sep_lines=1, charset=opts.charset)):
         if idx >= opts.start_index:
             metrics['tried'] += 1
             is_undefined = True
