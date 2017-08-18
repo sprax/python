7d5c3d717417ea3974a124ebbdb4057bc0173139
Sprax Lines <sprax@comcast.net>
2017-08-18 12:54:46 -0400

    scraper

---
+++ txt/scraper.py
@@ -0,0 +1,61 @@
+
+#!/usr/bin/env python3
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+def get_url_text(url):
+    # validate url?
+    try:
+        got = requests.get(url)
+        return got.text
+    except:
+        print("Could not open connection to {}".format(url))
+    return ""
+
+
+#css selectors:
+#non-main quizzes
+#.all-other-quizzes a
+def get_qa_pairs(text):
+    soup = BeautifulSoup(text, 'lxml')
+    question_nodes = soup.find_all(class_='quick-quiz-question')
+
+    qa_pairs = []
+    for node in question_nodes:
+        # Get question and answer texts
+        q_text = node.find_all('h3')[0].text.strip('1234567890. \s\t\n').strip().replace("\n", " ")
+        a_text = node.find_all(class_='border-blue semi-bold true-answer')[0].text.strip().replace("\n", " ")
+        qa_pairs.append((q_text, a_text))
+    return qa_pairs
+
+def get_quiz_relative_paths(text):
+    soup = BeautifulSoup(text, 'lxml')
+    link_nodes = soup.select(".all-other-quizzes a")
+
+    relative_paths = []
+    for node in link_nodes:
+        relative_paths.append(node.get('href'))
+    return relative_paths
+
+def csv_write_qa(qa_pairs, path):
+    ''' Write a list of (question, answer) tuples to a CSV file. '''
+    try:
+        with open(path, "w", newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            for pair in qa_pairs:
+                writer.writerow(pair)
+    except Exception as ex:
+        print("csv_write_qa failed to write qa_pairs to ({}) with error: {}".format(path, ex))
+
+def csv_read_qa(path):
+    ''' Return a list of (question, answer) tuples read from a CSV file. '''
+    qa_pairs = []
+    try:
+        with open(path, 'rt') as infile:
+            reader = csv.reader(infile)
+            for row in reader:
+                qa_pairs.append(row)
+    except Exception as ex:
+        print("csv_read_qa failed to get qa_pairs from ({}) with error: {}".format(path, ex))
+    return qa_pairs
