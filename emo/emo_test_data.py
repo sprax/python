d54f3bdda921200f5209d04f228229b39d3a9e54
Sprax Lines <sprax@comcast.net>
2017-09-15 01:53:27 -0400

    test data

--- emo/emo_test_data.py
+++ emo/emo_test_data.py
@@ -1,69 +1,69 @@
 
 
 SENTENCES = [
-    # "Wind and waves may rock the boat, but only you can tip the crew.",
-    # "I love you",
-    # "So it's the US vs. Canada in football, I mean soccer!?",
-    # "Lady Astor: “Winston, if I were your wife I’d put poison in your coffee.",
-    # "Winston Churchill: “Nancy, if I were your husband I’d drink it.",
-    # "I'm 100% sure <3 ain't a 4-letter word, even on Rhys' say-so?!",
-    # "When the eagles are silent, the parrots begin to jabber.",
-    # "If you have an important point to make, don’t try to be subtle or clever. Use a pile driver. Hit the point once. Then come back and hit it again. Then hit it a third time -- a tremendous whack.",
-    # "Success consists of going from failure to failure without loss of enthusiasm.",
-    # "Character may be manifested in the great moments, but it is made in the small ones.",
-    # "Men occasionally stumble over the truth, but most of them pick themselves up and hurry off as if nothing has happened.",
-    # "Don't give me no lip, you one-eye'd jack-ass!",
-    # "No ears or lip for you!",
-    # " _Wind And Waves and the Winds And my Wave_. ",
-    # "Art is the only way to run away without leaving home. -- Twyla Tharp",
-    # "If people knew how hard I worked to get my mastery, it wouldn't seem so wonderful at all. --Michelangelo",
-    # "I never paint dreams or nightmares. I paint my own reality. -- Frida Kahlo",
-    # "Whether you succeed or not is irrelevant, there is no such thing.  Making your unknown known is the important thing. -- Georgia O’Keeffe",
-    # "Art washes away from the soul the dust of everyday life. --Pablo Picasso",
-    # "They always say time changes things, but you actually have to change them yourself. --Andy Warhol",
-    # "Poor is the pupil who does not surpass his master. --Leonardo da Vinci",
-    # "I saw the angel in the marble and carved until I set him free. --Michelangelo",
-    # "For me, painting is a way to forget life.  It is a cry in the night, a strangled laugh. --Georges Rouault",
-    # "Art is the stored honey of the human soul. --Theodore Dreiser",
-    # "I would rather die of passion than of boredom. --Vincent van Gogh",
-    # "Go on, take the money and run.  --Steve Miller",
-    # " _Wind And Waves and the Winds And my Wave_. ",
-    # "Art is the only way to run away without leaving home. -- Twyla Tharp",
-    # "If people knew how hard I worked to get my mastery, it wouldn't seem so wonderful at all. --Michelangelo",
-    # "I never paint dreams or nightmares. I paint my own reality. -- Frida Kahlo",
-    # "Whether you succeed or not is irrelevant, there is no such thing.  Making your unknown known is the important thing. -- Georgia O’Keeffe",
-    # "Art washes away from the soul the dust of everyday life. --Pablo Picasso",
-    # "They always say time changes things, but you actually have to change them yourself. --Andy Warhol",
-    # "Poor is the pupil who does not surpass his master. --Leonardo da Vinci",
-    # "I saw the angel in the marble and carved until I set him free. --Michelangelo",
-    # "For me, painting is a way to forget life.  It is a cry in the night, a strangled laugh. --Georges Rouault",
-    # "Art is the stored honey of the human soul. --Theodore Dreiser",
-    # "I would rather die of passion than of boredom. --Vincent van Gogh",
-    # "Go on, take the money and run.  --Steve Miller",
-    #
-    # """Call me Ishmael. Some years ago--never mind how long precisely--having
-    # little or no money in my purse, and nothing particular to interest me on
-    # shore, I thought I would sail about a little and see the watery part of
-    # the world. It is a way I have of driving off the spleen and regulating
-    # the circulation. Whenever I find myself growing grim about the mouth;
-    # whenever it is a damp, drizzly November in my soul; whenever I find
-    # myself involuntarily pausing before coffin warehouses, and bringing up
-    # the rear of every funeral I meet; and especially whenever my hypos get
-    # such an upper hand of me, that it requires a strong moral principle to
-    # prevent me from deliberately stepping into the street, and methodically
-    # knocking people's hats off--then, I account it high time to get to sea
-    # as soon as I can. This is my substitute for pistol and ball. With a
-    # philosophical flourish Cato throws himself upon his sword; I quietly
-    # take to the ship. There is nothing surprising in this. If they but knew
-    # it, almost all men in their degree, some time or other, cherish very
-    # nearly the same feelings towards the ocean with me.""",
-    # "You sailed off on the boat I blew up.",
-    # "Ice ale da faun the bowtie blue up.",
-    # "You sail off on the boat I blow up.",
-    # "Gosh dang it for me if I ain't what's-its-name from Tie's nightmare bowling party?",
-    # "Go call my horses.",
-    # "You got a maelstrom of mail and email, you male female mailman.",
-    # "Call me Ishmael.",
+    "Wind and waves may rock the boat, but only you can tip the crew.",
+    "I love you",
+    "So it's the US vs. Canada in football, I mean soccer!?",
+    "Lady Astor: “Winston, if I were your wife I’d put poison in your coffee.",
+    "Winston Churchill: “Nancy, if I were your husband I’d drink it.",
+    "I'm 100% sure <3 ain't a 4-letter word, even on Rhys' say-so?!",
+    "When the eagles are silent, the parrots begin to jabber.",
+    "If you have an important point to make, don’t try to be subtle or clever. Use a pile driver. Hit the point once. Then come back and hit it again. Then hit it a third time -- a tremendous whack.",
+    "Success consists of going from failure to failure without loss of enthusiasm.",
+    "Character may be manifested in the great moments, but it is made in the small ones.",
+    "Men occasionally stumble over the truth, but most of them pick themselves up and hurry off as if nothing has happened.",
+    "Don't give me no lip, you one-eye'd jack-ass!",
+    "No ears or lip for you!",
+    " _Wind And Waves and the Winds And my Wave_. ",
+    "Art is the only way to run away without leaving home. -- Twyla Tharp",
+    "If people knew how hard I worked to get my mastery, it wouldn't seem so wonderful at all. --Michelangelo",
+    "I never paint dreams or nightmares. I paint my own reality. -- Frida Kahlo",
+    "Whether you succeed or not is irrelevant, there is no such thing.  Making your unknown known is the important thing. -- Georgia O’Keeffe",
+    "Art washes away from the soul the dust of everyday life. --Pablo Picasso",
+    "They always say time changes things, but you actually have to change them yourself. --Andy Warhol",
+    "Poor is the pupil who does not surpass his master. --Leonardo da Vinci",
+    "I saw the angel in the marble and carved until I set him free. --Michelangelo",
+    "For me, painting is a way to forget life.  It is a cry in the night, a strangled laugh. --Georges Rouault",
+    "Art is the stored honey of the human soul. --Theodore Dreiser",
+    "I would rather die of passion than of boredom. --Vincent van Gogh",
+    "Go on, take the money and run.  --Steve Miller",
+    " _Wind And Waves and the Winds And my Wave_. ",
+    "Art is the only way to run away without leaving home. -- Twyla Tharp",
+    "If people knew how hard I worked to get my mastery, it wouldn't seem so wonderful at all. --Michelangelo",
+    "I never paint dreams or nightmares. I paint my own reality. -- Frida Kahlo",
+    "Whether you succeed or not is irrelevant, there is no such thing.  Making your unknown known is the important thing. -- Georgia O’Keeffe",
+    "Art washes away from the soul the dust of everyday life. --Pablo Picasso",
+    "They always say time changes things, but you actually have to change them yourself. --Andy Warhol",
+    "Poor is the pupil who does not surpass his master. --Leonardo da Vinci",
+    "I saw the angel in the marble and carved until I set him free. --Michelangelo",
+    "For me, painting is a way to forget life.  It is a cry in the night, a strangled laugh. --Georges Rouault",
+    "Art is the stored honey of the human soul. --Theodore Dreiser",
+    "I would rather die of passion than of boredom. --Vincent van Gogh",
+    "Go on, take the money and run.  --Steve Miller",
+
+    """Call me Ishmael. Some years ago--never mind how long precisely--having
+    little or no money in my purse, and nothing particular to interest me on
+    shore, I thought I would sail about a little and see the watery part of
+    the world. It is a way I have of driving off the spleen and regulating
+    the circulation. Whenever I find myself growing grim about the mouth;
+    whenever it is a damp, drizzly November in my soul; whenever I find
+    myself involuntarily pausing before coffin warehouses, and bringing up
+    the rear of every funeral I meet; and especially whenever my hypos get
+    such an upper hand of me, that it requires a strong moral principle to
+    prevent me from deliberately stepping into the street, and methodically
+    knocking people's hats off--then, I account it high time to get to sea
+    as soon as I can. This is my substitute for pistol and ball. With a
+    philosophical flourish Cato throws himself upon his sword; I quietly
+    take to the ship. There is nothing surprising in this. If they but knew
+    it, almost all men in their degree, some time or other, cherish very
+    nearly the same feelings towards the ocean with me.""",
+    "You sailed off on the boat I blew up.",
+    "Ice ale da faun the bowtie blue up.",
+    "You sail off on the boat I blow up.",
+    "Gosh dang it for me if I ain't what's-its-name from Tie's nightmare bowling party?",
+    "Go call my horses.",
+    "You got a maelstrom of mail and email, you male female mailman.",
+    "Call me Ishmael.",
     "Haters gonna hate.",
     "Cartwheelers gotta cartwheel."
 ]
