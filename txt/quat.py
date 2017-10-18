967c8a75465aa7485a4de3a22b1af7d8aa49c12e
Sprax Lines <sprax@comcast.net>
2017-10-18 15:05:49 -0400

    pyl

--- txt/quat.py
+++ txt/quat.py
@@ -6,8 +6,13 @@ from collections import namedtuple
 
 Quat = namedtuple("Quat", "id question answer label")
 
-if __name__ == '__main__':
+def test():
+    '''Test Quat, a 4-tuple: (id, question, answer, label)'''
+
     quat1 = Quat(1, "What's up?", "Nothing.", 'salutation')
     print("quat1: ", quat1)
     quat2 = Quat(2, "What's that?", "I don't know.", 'indexical')
     print("quat2: ", quat2)
+
+if __name__ == '__main__':
+    test()
