e5a5f2e1c63eae4842ff25fa540db71ca94ebd86
Sprax Lines <sprax@comcast.net>
2017-10-25 20:50:18 -0400

    su.reorder_lines_and_ids

--- txt/sim_util.py
+++ txt/sim_util.py
@@ -17,7 +17,7 @@ import text_fio
 
 def sort_numbered_lines_with_blanks(inpath, outpath, verbose=True, charset='utf8'):
     '''read numbered lines and output them in sorted order, with blanks for skipped numbers.'''
-    lines = sorted(read_text_lines(inpath, charset=charset))
+    lines = sorted(text_fio.read_text_lines(inpath, charset=charset))
     prev = int(lines[0].split()[0]) - 1
     with open(outpath, "w") as outfile:
         for line in lines:
@@ -36,7 +36,7 @@ def sort_numbered_lines_with_blanks(inpath, outpath, verbose=True, charset='utf8
 def pack_lines_and_ids(inpath, outpath, offset=100, verbose=True, sep="\t", charset='utf8'):
     '''Renumber lines to fill in any gaps and output them in sorted order.'''
     lines, tran = [], {}
-    for idx, line in enumerate(read_text_lines(inpath, charset=charset)):
+    for idx, line in enumerate(text_fio.read_text_lines(inpath, charset=charset)):
         toks = line.split()
         lnum = int(toks[0])
         rnum = int(toks[-1])
@@ -59,31 +59,41 @@ def pack_lines_and_ids(inpath, outpath, offset=100, verbose=True, sep="\t", char
             print(lnum, line[4:-4], rnum, sep=sep, file=outfile)
 
 
-def reorder_lines_and_ids(inpath, outpath, offset=200, verbose=True, sep="\t", charset='utf8'):
+def reorder_lines_and_ids(inpath, outpath, devlen=194, offset=200, verbose=False, sep="\t", charset='utf8'):
     '''Renumber lines to give a constant offset between ID and refID (first and last fields as int).'''
-    lines, tran = [], {}
+    lines, tran, lnums = [], {}, set()
     for idx, line in enumerate(text_fio.read_text_lines(inpath, charset=charset)):
         toks = line.split()
         lnum = int(toks[0])
         rnum = int(toks[-1])
-        onum = lnum + offset
-        if idx < offset and rnum != onum:
-            if verbose:
-                print("RNUM != ONUM:", rnum, onum, "AT: ", line)
-            if rnum in tran:
-                raise Exception("ERROR: rnum %d already encountered!" % rnum)
+        if idx < devlen:
+            # change ref number to constant offset from id number (offset number)
+            lnums.add(lnum)
+            onum = lnum + offset
+            if rnum != onum:
+                if verbose:
+                    print("RNUM != ONUM:", rnum, onum, "AT: ", line)
+                if rnum in tran:
+                    raise Exception("ERROR: rnum %d already encountered!" % rnum)
             tran[rnum] = onum
-            rnum = onum
-        toks[0] = lnum
-        toks[-1] = rnum
-        lines.append((line, toks))
+            toks[0] = lnum
+            toks[-1] = onum
+            lines.append((line, toks))
+        else:
+            # change id number to matched changed ref number (offset number)
+            if lnum in tran:
+                toks[0] = tran[lnum]
+            elif rnum in lnums:
+                toks[0] = lnum
+            else:
+                raise Exception("ERROR: unknown lnum %d after index %d!" % (rnum, devlen))
 
+            toks[-1] = rnum
+            lines.append((line, toks))
+
+    lines.sort(key = lambda item: item[1])
     with open(outpath, "w") as outfile:
-        for idx, line, toks in enumerate(lines):
+        for line, toks in lines:
             lnum = toks[0]
             rnum = toks[-1]
-            # if idx < offset and rnum in tran:
-            #     rnum = tran[rnum]
-            if idx >= offset and lnum in tran:
-                lnum = tran[lnum]
             print(lnum, line[4:-4], rnum, sep=sep, file=outfile)
