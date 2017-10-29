ca1a3fd1bb4af21b97a7e51b40b35adec3dfe0f7
Sprax Lines <sprax@comcast.net>
2017-10-29 16:05:33 -0400

    test_handle_kbint

--- txt/kb_int.py
+++ txt/kb_int.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python
-import re
 import time
 
-rgxb = re.compile(r"^\s*['\"]")
+def handle_kbint(start_time, prev_kbint_time, message, location=None, inspect=True):
+    '''
+    Handle KeyboardInterrupt with just a method, not a class.
+    Usage (as inside a long-running loop):
+        start_time = prev_kbint = time.time()
+        try:
+            # do things
+        except KeyboardInterrupt:
+            seconds_since_kbint = handle_KeyboardInterrupt(start_time, prev_kbint_time, location, message)
+            break if seconds_since_kbint < 2 else prev_kbint_time += seconds_since_kbint
+    '''
+    kbint_time = time.time()
+    sec_since_start = kbint_time - start_time
+    sec_since_kbint = kbint_time - prev_kbint_time
+    if location is None:
+        if inspect:
+            import inspect
+            location = inspect.stack()[1][3]
+        else:
+            location = ''
+    print("\nKeyboardInterrupt at %d seconds, %d since prior: %s: %s" % (
+          sec_since_start, sec_since_kbint, location, message))
+    return sec_since_kbint
 
-def tr(ex):
-    if (ex):
-            print('True')
-    else:
-            print('false')
 
-sp = "'Why, it's what I'm obliged to keep a little of in the house, to putinto the blessed infants' Daffy , when they ain't well, Mr. Bumble,'replied Mrs. Mann as she opened a corner cupboard, and took down abottle and glass.  'It's gin.  I'll not deceive you, Mr. B.  It's gin.'"
-
-def kbint():
-    count = 0
+def test_handle_kbint():
+    '''test_handle_kbint: Since the KeyboardInterrupt will be caught during
+    the sleep(1), the printed odd count will lag 1 behind the even count.'''
+    count = even = odd = 0
+    start_time = prev_kbint = time.time()
     print("kbint BEG", count)
     while True:
         try:
-            time.sleep(1)
             count += 1
+            even = count * 2
+            time.sleep(1)
+            odd = even + 1
         except KeyboardInterrupt:
-            print("\nCaught KeyboardInterrupt at", count)
-            if count > 5:
-                break;
+            sec_since_kbint = handle_kbint(start_time, prev_kbint,
+                "Count %d,  Even %d,  Odd %d." % (count, even, odd))
+            if sec_since_kbint < 1:
+                print("Caught 2 interrupts in less than a second -- breaking out of loop in test_handle_kbint!")
+                break
+            prev_kbint += sec_since_kbint
     print("kbint END", count)
