a9731354240369f2d48c4875dadd63d222fd530c
Sprax Lines <sprax@comcast.net>
2017-11-28 23:24:39 -0500

    fix

--- romanize.py
+++ romanize.py
@@ -1,15 +1,17 @@
-NUMERAL = (
-    ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100),("XC", 90),
-    ("L", 50),("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1))
+'''Convert decimal number to Roman numerals'''
 
-def romanize(n):
+NUMERAL = (
+    ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90),
+    ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1))
+
+def romanize(nin):
+    '''Roman numeral representation of number'''
     roman = []
     for ltr, num in NUMERAL:
-       (k,n) = divmod(n, num)
-       roman.append(ltr*k)
+        (div, nin) = divmod(nin, num)
+        roman.append(ltr * div)
     return "".join(roman)
 
 
 YEAR = 1949
 print(YEAR, romanize(YEAR))
-
