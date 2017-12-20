e78753589cf1441c0facc8c6171186044cb1e233
Sprax Lines <sprax@comcast.net>
2017-12-20 00:00:08 -0500

    reverse

--- reverse.py
+++ reverse.py
@@ -6,6 +6,10 @@ def reverse_string(string):
     '''reverse'''
     return string[::-1]
 
-if __name__ == '__main__':
+def main():
+    '''test reverse_string'''
     string = sys.argv[1] if len(sys.argv) > 1 else 'Jesus Mary and Joseph'
     print(string, '==>', reverse_string(string))
+
+if __name__ == '__main__':
+    main()
