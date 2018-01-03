ea11b94e170f474842b7e7d60f42500e1e99ba4e
Sprax Lines <sprax@comcast.net>
2018-01-02 23:58:17 -0500

    lint

--- emo/sylcount.py
+++ emo/sylcount.py
@@ -10,13 +10,9 @@ Plan:
 '''
 
 import argparse
-import random
 import re
-import string
-from collections import defaultdict
 from collections import namedtuple
 from nltk.corpus import cmudict
-import emoji
 import emotuples as ET
 import text_regex as tregex
 
@@ -56,7 +52,7 @@ EXAMPLES = {
     #   1   2        3   5 6 7  8 9 0 1 2   3  4        5      6      7      8  9     0    1
     '"Ain\'t all y\'all young\'uns under 17," she\'d said, "like 11- or 15-years old?!"' : (13, 21),
     #  1     2      3    4     5   6  7  +3     1     2      3   +3  7  +2  0    1}
-    "Didn't you know my X-15's XLR-99 engine burned 15,000 pounds (6,717 kg) of propellant in 87 seconds?" : (17, 35),
+    "Didn't you know my X-15's XLR-99 engine burned 15,000 pounds (6,717 kg) of propellant in 87 seconds?": (17, 35),
     # 1  2   3    4   5 6 +2    +3 +2 4  5    6     +2  +2  1     +3  +8 +3  6    7 8  9   0  +3  4 5
 }
 
@@ -140,6 +136,7 @@ def phone_sex_cmu(cmu_prons, lwrd, verbose):
     return phone_sex
 
 def gen_word_phone_sex(prons, emots, verbose=1):
+    '''generates word phonetic sequences'''
     word_phone_sex = {}
     for tup in emots:
         if tup.flags > 0:
@@ -167,18 +164,19 @@ def main():
 
     cmu_prons = cmudict.dict() # get the CMU Pronouncing Dict
 
-    for sentence, counts in EXAMPLES.items():
-        print("MANUAL", counts[0], sentence)
-        tokens = tregex.word_tokens(sentence)
-        print("TOKENS", len(tokens), tokens)
-        swords = tregex.word_splits(sentence)
-        print("SPLITS", len(swords), swords)
-        scount = syl_count_sum(cmu_prons, tokens)
-        vcount = count_vowel_groups(sentence)
-        fcount = count_vowels_first_last(sentence)
-        print("SYLLABLES:  manual(%d)  cmupro(%d)  vowelg(%d)  nrules(%d)" % (
-            counts[1], scount, vcount, fcount))
-        print()
+    if args.verbose > 1:
+        for sentence, counts in EXAMPLES.items():
+            print("MANUAL", counts[0], sentence)
+            tokens = tregex.word_tokens(sentence)
+            print("TOKENS", len(tokens), tokens)
+            swords = tregex.word_splits(sentence)
+            print("SPLITS", len(swords), swords)
+            scount = syl_count_sum(cmu_prons, tokens)
+            vcount = count_vowel_groups(sentence)
+            fcount = count_vowels_first_last(sentence)
+            print("SYLLABLES:  manual(%d)  cmupro(%d)  vowelg(%d)  nrules(%d)" % (
+                counts[1], scount, vcount, fcount))
+            print()
 
     emotuples = ET.EmoTuples()
     nemos = gen_word_phone_sex(cmu_prons, emotuples.nemo_tuples)
