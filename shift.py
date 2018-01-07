0ad5bd0d1c530fa2d77ba97885a92b11be451ed1
Sprax Lines <sprax@comcast.net>
2018-01-06 23:58:45 -0500

    STRING

--- shift.py
+++ shift.py
@@ -8,5 +8,5 @@ def reverse_string(string):
     return string[::-2] + string[0:-1][::-2]
 
 if __name__ == '__main__':
-    string = sys.argv[1] if len(sys.argv) > 1 else 'Jesus Mary and Joseph'
-    print(string, '==>', reverse_string(string))
+    STRING = sys.argv[1] if len(sys.argv) > 1 else 'Jesus Mary and Joseph'
+    print(STRING, '==>', reverse_string(STRING))
