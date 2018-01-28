a866c550dfbe2a8f26f4f3294e88e8d63a86e650
Sprax Lines <sprax@comcast.net>
2018-01-27 22:42:07 -0500

    reverse every other

--- shift.py
+++ shift.py
@@ -2,11 +2,11 @@
 
 import sys
 
-def reverse_string(string):
+def shift_string(string):
     '''shift ==> tisfh'''
     # return string[::-2] + string[1:-1:-2]
     return string[::-2] + string[0:-1][::-2]
 
 if __name__ == '__main__':
     STRING = sys.argv[1] if len(sys.argv) > 1 else 'Jesus Mary and Joseph'
-    print(STRING, '==>', reverse_string(STRING))
+    print(STRING, '==>', shift_string(STRING))
