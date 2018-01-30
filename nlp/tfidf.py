2e998dda042c7ebaec8ccd35622fd8ff78ebe85c
Sprax Lines <sprax@comcast.net>
2018-01-30 17:02:01 -0500

    whiteboard

---
+++ nlp/tfidf.py
@@ -0,0 +1,37 @@
+
+#!/usr/bin/env python3
+# Sprax Lines       2018.01      Python 3.5
+'''Compute TFIDF matrix on a list of vectorized documents'''
+
+
+def tfidf_doc_list(doc_list):
+    ''' Input: list of term-frequency vectors representing documents.
+        Ouput: list of TF-IDF vectors representing compared documents
+        (same dimensions as input)
+    '''
+    ntokens = len(doc_list[0])  # count of all unique tokens
+    doxfreq = [0] * ntokens     # count
+    for doc in doc_list:
+        for idx, count in enumerate(doc):
+            if count > 0:
+                doxfreq[idx] += 1
+    for doc in doc_list:
+        for idx, count in enumerate(doc):
+            doc[idx] /= doxfreq[idx]
+    return doc_list
+
+
+def test_tfidf_doc_list_1(ntokens=5):
+    '''test that tfidf_doc_list gives the correct output for "idendity matrix" input'''
+    doc_list = []
+    for idx in range(ntokens):
+        lst = [0] * ntokens
+        lst[idx] = 1
+        doc_list.append(lst)
+
+    result = tfidf_doc_list(doc_list)
+    expect = doc_list
+    assert result == expect
+
+
+test_tfidf_doc_list_1(6)
