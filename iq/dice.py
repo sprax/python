b754c6f2d67ba6310a295a29554f02dcb443ec97
Sprax Lines <sprax@comcast.net>
2018-02-10 20:30:46 -0700

    clean

--- iq/dice.py
+++ iq/dice.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+''' Sums of two random dice throws:
+2   1   11
+3   2   12, 21
+4   3   13, 31, 22
+5   4   14, 23, 32, 41
+6   5   15, 24, 33, 42, 51
+7   6   16, 25, 34, 43, 52, 61
+8   5   26, 35, 44, 53, 62
+9   4   36, 45, 54, 63
+10  3   46, 55, 64
+11  2   56, 65
+12  1   66
+-- --
+   36
+'''
 import argparse
 import itertools
 import random
@@ -8,22 +23,9 @@ PAIR_SUMS = [
     2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7,
     7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 11, 11, 12]
 
+
 def dice_pair_sum():
-    ''' Sums of two random dice throws:
-    2   1   11
-    3   2   12, 21
-    4   3   13, 31, 22
-    5   4   14, 23, 32, 41
-    6   5   15, 24, 33, 42, 51
-    7   6   16, 25, 34, 43, 52, 61
-    8   5   26, 35, 44, 53, 62
-    9   4   36, 45, 54, 63
-    10  3   46, 55, 64
-    11  2   56, 65
-    12  1   66
-    -- --
-       36
-    '''
+    ''' Sum value from the one throw of a pair of dice '''
     rdx = random.randint(0, 35)
     return PAIR_SUMS[rdx]
 
@@ -36,7 +38,6 @@ def dice_pair_sum_gen():
 
 def main():
     ''' driver for dice_pair_sum_gen'''
-    default_file = '../words.txt'
     parser = argparse.ArgumentParser(description=dice_pair_sum_gen.__doc__)
     parser.add_argument('count', type=int, nargs='?', const=16, default=10,
                         help='Count of randomly sampled dice pair sums (const: 16, default: 10)')
