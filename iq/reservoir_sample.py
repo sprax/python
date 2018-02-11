28a44f0f4dba2cfc1df5cde7e4134483384be8dd
Sprax Lines <sprax@comcast.net>
2018-02-10 18:22:07 -0700

    seed

--- iq/reservoir_sample.py
+++ iq/reservoir_sample.py
@@ -7,19 +7,19 @@ DEFAULT_SEED = 12345
 def sample_line_items(text_file, count=SAMPLE_COUNT, seed=DEFAULT_SEED, verbose=1):
     ''' extracts uniformly random sample of count lines from the text file '''
     random.seed(seed)                   # for repeatability
-    sample_lines = []
-    for index, line in enumerate(open("../words.txt")):
+    reservoir = []
+    for index, line in enumerate(open(text_file)):
         # Generate the reservoir
         if index < count:
-            sample_lines.append(line.rstrip())
+            reservoir.append(line.rstrip())
         else:
             # Randomly replace elements in the reservoir
             # with a decreasing probability.
             # Choose an integer between 0 and index (inclusive)
-            r = random.randint(0, index)
-            if r < count:
-                sample_lines[r] = line.rstrip()
-    print(sample_lines)
+            rdx = random.randint(0, index)
+            if rdx < count:
+                reservoir[rdx] = line.rstrip()
+    print(reservoir)
 
 
 def main():
@@ -30,13 +30,15 @@ def main():
                         help='Count of randomly sampled lines (const: 16, default: 10)')
     parser.add_argument('file', type=str, nargs='?', default=default_file,
                         help="Text file, one item per line (default: %s)" % default_file)
+    parser.add_argument('-seed', type=int, nargs='?', const=1, default=12345,
+                        help='seed for random (const: 1,  default: 12345)')
     parser.add_argument('-verbose', type=int, nargs='?', const=1, default=1,
                         help='verbosity of output (default: 1)')
     args = parser.parse_args()
 
     print("args.count:", args.count)
 
-    sample_line_items(args.file, args.count)
+    sample_line_items(args.file, args.count, args.seed, args.verbose)
 
 
 if __name__ == '__main__':
