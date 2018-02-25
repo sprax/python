76b835c622db23eab00930bd2d5376cc9dca814a
Sprax Lines <sprax@comcast.net>
2018-02-25 10:08:21 -0500

    lint

--- tf/add.py
+++ tf/add.py
@@ -3,27 +3,30 @@
 #   Your CPU supports instructions that this TensorFlow binary was not compiled to use: SSE4.2 AVX AVX2 FMA
 '''
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-'''
-'''
-# import tensorflow
 import tensorflow as tf
 
 
-# build computational graph
-a = tf.placeholder(tf.int16)
-b = tf.placeholder(tf.int16)
+def main():
+    '''Add two numbers in TF'''
 
-addition = tf.add(a, b)
+    # suppress sub-optimal build warning:
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# initialize variables
-# init = tf.initialize_all_variables()
-init = tf.global_variables_initializer()
+    # build computational graph
+    v_a = tf.placeholder(tf.int16)
+    v_b = tf.placeholder(tf.int16)
+    addition = tf.add(v_a, v_b)
 
-# create session and run the graph
-with tf.Session() as sess:
-    sess.run(init)
-    print("Addition: %i" % sess.run(addition, feed_dict={a: 2, b: 3}))
+    # initialize variables
+    # init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
 
-# close session
-sess.close()
+    # create session and run the graph
+    with tf.Session() as sess:
+        sess.run(init)
+        ans = sess.run(addition, feed_dict={v_a: 2, v_b: 3})
+        print("Addition:", ans)
+
+
+if __name__ == '__main__':
+    main()
