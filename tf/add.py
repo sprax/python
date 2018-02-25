5dffc64341d245c57901310b16169ae843ff4c63
Sprax Lines <sprax@comcast.net>
2018-02-24 23:18:58 -0500

    warnings

--- tf/add.py
+++ tf/add.py
@@ -1,5 +1,14 @@
+'''
+# To disable warning:
+#   Your CPU supports instructions that this TensorFlow binary was not compiled to use: SSE4.2 AVX AVX2 FMA
+'''
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+'''
+'''
 # import tensorflow
 import tensorflow as tf
+
 
 # build computational graph
 a = tf.placeholder(tf.int16)
@@ -8,7 +17,8 @@ b = tf.placeholder(tf.int16)
 addition = tf.add(a, b)
 
 # initialize variables
-init = tf.initialize_all_variables()
+# init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 # create session and run the graph
 with tf.Session() as sess:
