fccb8e9090b75d8b009c59f82078154fd504221c
Sprax Lines <sprax@comcast.net>
2018-03-17 09:31:09 -0400

    format

--- la/null_space.py
+++ la/null_space.py
@@ -9,9 +9,9 @@ import numpy as np
 from scipy import linalg, matrix
 
 def null(A, eps=1e-12):
-    u, s, vh = scipy.linalg.svd(A)
-    padding = max(0,np.shape(A)[1]-np.shape(s)[0])
-    null_mask = np.concatenate(((s <= eps), np.ones((padding,),dtype=bool)),axis=0)
+    u, s, vh = scipy.linalg.svd(A)   # , full_matrices=False)
+    padding = max(0, np.shape(A)[1]-np.shape(s)[0])
+    null_mask = np.concatenate(((s <= eps), np.ones((padding,), dtype=bool)), axis=0)
     null_space = scipy.compress(null_mask, vh, axis=0)
     return scipy.transpose(null_space)
 
@@ -36,8 +36,8 @@ show_null(A)
 # [[  4.44089210e-16]
 # [  6.66133815e-16]]
 
-A = matrix([[1,0,1,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]])
-show_null(A)
+B = matrix([[1,0,1,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]])
+show_null(B)
 # print A * null(A)
 # [[ 0.  0.]
 #  [ 0.  0.]
