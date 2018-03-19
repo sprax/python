1c6707dcc18d35c88b14585d64217941d6de1633
Sprax Lines <sprax@comcast.net>
2018-03-18 23:51:55 -0400

    space

--- la/null_space.py
+++ la/null_space.py
@@ -11,7 +11,7 @@ from scipy import linalg, matrix
 def null(A, eps=1e-12):
     u, s, vh = scipy.linalg.svd(A)   # , full_matrices=False)
     padding = max(0, np.shape(A)[1]-np.shape(s)[0])
-    null_mask = np.concatenate(((s <= eps), np.ones((padding,), dtype=bool)), axis=0)
+    null_mask = np.concatenate(((s <= eps), np.ones((padding, ), dtype=bool)), axis=0)
     null_space = scipy.compress(null_mask, vh, axis=0)
     return scipy.transpose(null_space)
 
@@ -21,25 +21,28 @@ def show_null(A):
     print "null(A):", null(A)
     print "A * null(A):", A * null(A)
 
-A = matrix([
-    [2,  3, 5],
-    [-4, 2, 3]
-])
-show_null(A)
+def main():
+    A = matrix([
+        [2, 3, 5],
+        [-4, 2, 3]
+    ])
+    show_null(A)
 
-# [[ 0.         -0.70710678]
-#  [ 0.          0.        ]
-#  [ 0.          0.70710678]
-#  [ 1.          0.        ]]
+    # [[ 0.         -0.70710678]
+    #  [ 0.          0.        ]
+    #  [ 0.          0.70710678]
+    #  [ 1.          0.        ]]
 
-# null(A):
-# [[  4.44089210e-16]
-# [  6.66133815e-16]]
+    # null(A):
+    # [[  4.44089210e-16]
+    # [  6.66133815e-16]]
 
-B = matrix([[1,0,1,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]])
-show_null(B)
-# print A * null(A)
-# [[ 0.  0.]
-#  [ 0.  0.]
-#  [ 0.  0.]
-#  [ 0.  0.]]
+    B = matrix([[1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    show_null(B)
+    # print A * null(A)
+    # [[ 0.  0.]
+    #  [ 0.  0.]
+    #  [ 0.  0.]
+    #  [ 0.  0.]]
+
+main()
