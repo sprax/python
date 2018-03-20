c1d399854a7088cd52ab8896679c11630b387507
Sprax Lines <sprax@comcast.net>
2018-03-20 00:19:46 -0400

    test kadane

--- kadane.py
+++ kadane.py
@@ -1,6 +1,8 @@
 # kadane.py from the interwebs
 '''Kadane algorithm for finding the maximum sum of a contiguous subarray'''
 
+from __future__ import print_function
+
 def max_contiguous_sum(array):
     '''function to find the maximum sum of a contiguous subarray'''
     max_so_far = max_ending_here = 0
@@ -8,3 +10,12 @@ def max_contiguous_sum(array):
         max_ending_here = max(0, max_ending_here + elt)
         max_so_far = max(max_so_far, max_ending_here)
     return max_so_far
+
+def main():
+    '''test max_contiguous_sum (Kadane algorithm)'''
+    arr = [1, 2, -3, 1, 3, 1, -2, 2, -1, 2]
+    mcs = max_contiguous_sum(arr)
+    print(arr, " ==> ", mcs, "(expecting 7)")
+
+if __name__ == '__main__':
+    main()
