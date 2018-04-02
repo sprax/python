8ad69783ce5623c246cca92c4d0cbf5edeef17d1
Sprax Lines <sprax@comcast.net>
2018-04-01 22:00:08 -0400

    test_1_10_100_1000

--- iq/log_unif.py
+++ iq/log_unif.py
@@ -22,22 +22,39 @@ def random_log_uniform(lo_exp, hi_exp, base=10, offset=0):
 def test_1_10_100(count):
     ''' test
     '''
-    range_1_100 = [random_log_uniform(0, 2) for x in range(count)]
-    range_1_10 = [x for x in range_1_100 if x < 10]
+    range_all = [random_log_uniform(0, 2) for x in range(count)]
+    range_1_10 = [x for x in range_all if x < 10]
     count_1_10 = len(range_1_10)
-    range_10_100 = [x for x in range_1_100 if x >= 10]
+    range_10_100 = [x for x in range_all if x >= 10]
     count_10_100 = len(range_10_100)
+    print("\n%d log-uniform samples partitioned into [1, 10) and [10, 100):" % count)
     print("count in [1, 10): %d" % count_1_10)
     print("count in [10, 100): %d" % count_10_100)
-
     return count_1_10, count_10_100
 
 
+def test_1_10_100_1000(count):
+    ''' test
+    '''
+    range_all = [random_log_uniform(0, 3) for x in range(count)]
+    range_1_10 = [x for x in range_all if x < 10]
+    count_1_10 = len(range_1_10)
+    range_10_100 = [x for x in range_all if x >= 10 and x < 100]
+    count_10_100 = len(range_10_100)
+    range_100_1000 = [x for x in range_all if x >= 100]
+    count_100_1000 = len(range_100_1000)
+    print("\n%d log-uniform samples partitioned into [1, 10), [10, 100), and [100, 1000):" % count)
+    print("count in [1, 10): %d" % count_1_10)
+    print("count in [10, 100): %d" % count_10_100)
+    print("count in [100, 1000): %d]" % count_100_1000)
+    return count_1_10, count_10_100
 
 
 def unit_test(args):
     ''' unit test '''
     test_1_10_100(args.count)
+    test_1_10_100_1000(args.count)
+    print()
 
 
 def main():
