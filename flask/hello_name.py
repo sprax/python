1f32e03540d992265ac913a2fcfcd7f7aefcb325
Sprax Lines <sprax@comcast.net>
2018-04-20 01:18:06 +0200

    log order as datetime file

--- flask/hello_name.py
+++ flask/hello_name.py
@@ -1,5 +1,11 @@
 
+from __future__ import print_function
+from datetime import datetime
+import re
+import time
 from flask import Flask, flash, redirect, render_template, request, session, abort
+
+ORDER=0
 
 app = Flask(__name__)
 
@@ -9,10 +15,18 @@ def index():
 
 @app.route("/hello/<string:name>/")
 def hello(name):
+    # side effect:
+    global ORDER
+    date = datetime.now()
+    tstm = date.timetuple()
+    print(tstm)
+    dstr = time.strftime('%Y.%m.%d_%M_%S', tstm)
+    ORDER += 1
+    with open("ord%2d__%s.txt" % (ORDER, dstr), "w") as logf:
+        print(dstr, file=logf)
     return render_template(
         'test.html',name=name)
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=80)
     app.run(host='0.0.0.0', port=5000)
-
