dba6a1a3ecbb1abe80c005d5e167a41aa97a11f1
Sprax Lines <sprax@comcast.net>
2018-07-11 01:31:05 -0400

    pass in sup

--- num/fibonacci_list.py
+++ num/fibonacci_list.py
@@ -1,13 +1,18 @@
 # fibonacci_list.py -- tested with python 2.7 and 3.5
 '''list method for computing Fibonacci numbers up to given bound'''
+import sys
+
 def fib(sup): # return Fibonacci series up to sup
     """Return a list containing the Fibonacci series up to sup."""
     result = []
     a, b = 0, 1
-    while a < sup:
+    while a <= sup:
         result.append(a)    # see below
         a, b = b, a + b
     return result
 
-F100 = fib(100) # call the function
-print(F100)     # print the result
+
+if __name__ == '__main__':
+    num = int(sys.argv[1]) if len(sys.argv) > 1 else 89
+    fibs = fib(num) # call the function
+    print(fibs)     # print the result
