8018b43f12f904721ad65c8d54e1e97df6ce6873
Sprax Lines <sprax@comcast.net>
2018-07-12 13:24:11 -0400

    assertDictContainsSubset

--- term/test_eg.py
+++ term/test_eg.py
@@ -13,9 +13,19 @@ class TestExamples(unittest.TestCase):
         self.assertAlmostEqual(10.0, 9.99999999)
         self.assertAlmostEqual( 1.0, 0.99999999)
         self.assertNotAlmostEqual( 1.0, 0.9999999)
-        self.assertEqual( 1.0, 1.0 )
         self.assertNotEqual( 1.0, 0.999999999 )
+        self.assertEqual( 1.0, 1.0 )
         self.assertEqual(True, True)
+        lst = ['A', 1, 'bb', 'aa']
+        dct = { k : n for n, k in enumerate(lst) }
+        evn = { k : n for n, k in enumerate(lst) if n % 2 == 0 }
+        odd = { k : n for n, k in enumerate(lst) if n % 2 }
+        print("lst:", lst)
+        print("dct:", dct)
+        print("evn:", evn)
+        print("odd:", odd)
+        self.assertDictContainsSubset( evn, dct )
+        self.assertDictContainsSubset( odd, dct )
 
 
 
