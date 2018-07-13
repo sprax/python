bb524b81cb92c6dc93bd1cd7a201f01cea32d5f2
Sprax Lines <sprax@comcast.net>
2018-07-13 01:04:26 -0400

    circle

--- term/test_eg.py
+++ term/test_eg.py
@@ -1,9 +1,9 @@
-import unittest
-from flask import Flask, render_template, request
-# import flaskapi
-import requests
 import json
 import sys
+import unittest
+import requests
+from flask import Flask, render_template, request
+# import flaskapi
 
 class TestExamples(unittest.TestCase):
     ''' example tests '''
@@ -11,21 +11,23 @@ class TestExamples(unittest.TestCase):
     def test_asserts(self):
         ''' test TestCase's assert* functions '''
         self.assertAlmostEqual(10.0, 9.99999999)
-        self.assertAlmostEqual( 1.0, 0.99999999)
-        self.assertNotAlmostEqual( 1.0, 0.9999999)
-        self.assertNotEqual( 1.0, 0.999999999 )
-        self.assertEqual( 1.0, 1.0 )
+        self.assertAlmostEqual(1.0, 0.99999999)
+        self.assertNotAlmostEqual(1.0, 0.9999999)
+        self.assertNotEqual(1.0, 0.999999999)
+        self.assertEqual(1.0, 1.0)
         self.assertEqual(True, True)
         lst = ['A', 1, 'bb', 'aa']
-        dct = { k : n for n, k in enumerate(lst) }
-        evn = { k : n for n, k in enumerate(lst) if n % 2 == 0 }
-        odd = { k : n for n, k in enumerate(lst) if n % 2 }
+        dct = { k : n for n, k in enumerate(lst)}
+        evn = { k : n for n, k in enumerate(lst) if n % 2 == 0}
+        odd = { k : n for n, k in enumerate(lst) if n % 2}
         print("lst:", lst)
         print("dct:", dct)
         print("evn:", evn)
         print("odd:", odd)
-        self.assertDictContainsSubset( evn, dct )
-        self.assertDictContainsSubset( odd, dct )
+        self.assertDictContainsSubset(evn, dct)
+        self.assertDictContainsSubset(odd, dct)
+        self.assertNotIn('B', lst)
+        self.assertNotIn(['cc', 'DD'], lst)
 
 
 
