9b6646ca49eded96189688a98dc4637fa1f44acd
Sprax Lines <sprax@comcast.net>
2018-07-14 23:52:48 -0400

    lint

--- num/fibonacci_list.py
+++ num/fibonacci_list.py
@@ -13,6 +13,6 @@ def fib(sup): # return Fibonacci series up to sup
 
 
 if __name__ == '__main__':
-    num = int(sys.argv[1]) if len(sys.argv) > 1 else 89
-    fibs = fib(num) # call the function
-    print(fibs)     # print the result
+    NUM = int(sys.argv[1]) if len(sys.argv) > 1 else 89
+    FIBS = fib(NUM) # call the function
+    print(FIBS)     # print the result
