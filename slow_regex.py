bc0c907b2f58faeb6e8454e1c80a289bdabf0670
Sprax Lines <sprax@comcast.net>
2018-07-19 21:41:14 -0400

    lint

--- slow_regex.py
+++ slow_regex.py
@@ -1,16 +1,23 @@
-
+''' benchmark '''
 import timeit
 import re
 
-def one():
-        any(s in mystring for s in ('foo', 'bar', 'hello'))
+def one(mystring):
+    ''' tester '''
+    any(s in mystring for s in ('foo', 'bar', 'hello'))
 
 r = re.compile('(foo|bar|hello)')
-def two():
-        r.search(mystring)
+def two(mystring):
+    ''' tester '''
+    r.search(mystring)
 
+def main():
+    ''' test function '''
+    mystring = "hello"*1000
+    print([timeit.timeit(k(mystring), number=10000) for k in (one, two)])
+    mystring = "goodbye"*1000
+    print([timeit.timeit(k(mystring), number=10000) for k in (one, two)])
+    print(mystring)
 
-mystring="hello"*1000
-print([timeit.timeit(k, number=10000) for k in (one, two)])
-mystring="goodbye"*1000
-print([timeit.timeit(k, number=10000) for k in (one, two)])
+if __name__ == '__main__':
+    main()
