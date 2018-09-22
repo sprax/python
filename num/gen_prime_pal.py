242aa6ede3c5f3a7484827cf0c0819917dc7696a
Sprax Lines <sprax@comcast.net>
2018-09-22 14:03:53 -0400

    lint + main

--- num/gen_prime_pal.py
+++ num/gen_prime_pal.py
@@ -6,6 +6,7 @@ wrapper by sprax 2018.09, based on:
     http://code.activestate.com/recipes/117119/
 '''
 import itertools
+import sys
 
 def gen_primes():
     """ Generate an infinite sequence of prime numbers.
@@ -14,32 +15,28 @@ def gen_primes():
     # This is memory efficient, as the sieve is not "run forward"
     # indefinitely, but only as long as required by the current
     # number being tested.
-    #
-    D = {}
+    prime_divs = {}
 
-    # The running integer that's checked for primeness
-    q = 2
 
+    cand = 2    # The loop var to increment and check for primeness
     while True:
-        if q not in D:
-            # q is a new prime.
+        if cand not in prime_divs:
+            # cand is a new prime.
             # Yield it and mark its first multiple that isn't
             # already marked in previous iterations
-            #
-            yield q
-            D[q * q] = [q]
+            yield cand
+            prime_divs[cand * cand] = [cand]
         else:
-            # q is composite. D[q] is the list of primes that
-            # divide it. Since we've reached q, we no longer
+            # cand is composite. prime_divs[cand] is the list of primes that
+            # divide it. Since we've reached cand, we no longer
             # need it in the map, but we'll mark the next
             # multiples of its witnesses to prepare for larger
             # numbers
-            #
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
+            for prim in prime_divs[cand]:
+                prime_divs.setdefault(prim + cand, []).append(prim)
+            del prime_divs[cand]
+        cand += 1
 
-        q += 1
 
 def gen_prime_pal():
     """ Generate an infinite sequence of palindromic prime numbers,
@@ -61,3 +58,18 @@ def gen_prime_pal_idx_range(beg_idx=0, end_idx=1000):
         (x, s, n, n//2 if n%2 else (n-1)//2) for x, s, n in (
             (x, str(x), len(str(x))) for x in itertools.islice(
                 gen_primes(), beg_idx, end_idx))) if s[:n//2] == s[-1:h:-1])
+
+
+DEFAULT_BEG = 100
+DEFAULT_END = 2000
+
+def main():
+    '''Print lists of prime palindromes (default up to X)'''
+    argc = len(sys.argv)
+    beg_num = int(sys.argv[2]) if argc > 2 else DEFAULT_BEG
+    end_num = int(sys.argv[1]) if argc > 1 else DEFAULT_END
+
+    print list(gen_prime_pal_idx_range(beg_num, end_num))
+
+if __name__ == '__main__':
+    main()
