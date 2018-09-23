c3afd085fee7be44162ef4d0942cb1e00cb683c5
Sprax Lines <sprax@comcast.net>
2018-09-23 02:42:28 -0400

    lint

--- num/weirdnum.py
+++ num/weirdnum.py
@@ -4,19 +4,21 @@
 from __future__ import print_function
 import sys
 
-vinfo = sys.version_info
-v_major = vinfo[0]
-v_minor = vinfo[1]
-ver_str = "Python%d.%d" % (v_major, v_minor)
-print(ver_str, sys.argv[0])
+def main():
+    '''main'''
+    vinfo = sys.version_info
+    v_major = vinfo[0]
+    v_minor = vinfo[1]
+    ver_str = "Python%d.%d" % (v_major, v_minor)
+    print(ver_str, sys.argv[0])
+    if v_major < 3:
+        print("input function needs Python 3!   exit(1)")
+        exit(1)
 
+    num = int(input().strip())
+    if num % 2 == 0:
+        if num < 6 or num > 20:
+            print('Not ', end='')
+    print('Weird')
 
-if v_major < 3:
-    print("input function needs Python 3!")
-    exit(1)
-
-N = int(input().strip())
-if N % 2 == 0:
-    if N < 6 or N > 20:
-        print('Not ', end='')
-print('Weird')
+main()
