dde4393440e2ea0533864b0ac7c72f2dd05435d5
sprax <sprax@comcast.net>
2019-05-24 23:56:20 -0400

    cptr_matrix.py

---
+++ iq/cptr_matrix.py
@@ -0,0 +1,10 @@
+
+'''
+Given matrix in which each cell is either filled with 'C' (Computer) or empty.
+Computers can communicate if they are in the same row or in the same column. 
+Computers can also communicate through middleware computers. 
+Given index of a computer, find all computers it can communicate with. 
+How many communication paths are there? 
+Find the order of turning computers off, in which every computer will manage to communicate with another computer
+in order to pass the data it stores before it is turned off, keeping minimum computers on in the end.
+'''
