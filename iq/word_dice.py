4753745e8dc635a370248c8a70c7896e272a0cbe
sprax <sprax@comcast.net>
2019-05-25 00:07:05 -0400

    statement

---
+++ iq/word_dice.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python3
+'''
+Given a word of length n, and n six-sided dice with a character in each side,
+find out if this word is constructible by the set of given dice
+'''
+from __future__ import print_function
+
+import argparse
+# import pdb
+# from pdb import set_trace
+import random
