49082a0686e469eaf6b5b1a5977aee2af72821f7
sprax <sprax@comcast.net>
2019-08-03 00:18:45 -0400

    ngram

---
+++ iq/cf_engram.py
@@ -0,0 +1,8 @@
+#!/usr/env python'''
+Compare two documents(string array) based on n grams. 
+e.g doc1 – Today is Sunday. 
+doc2 – Today is Saturday 
+if n = 2 then number of duplicates is 1 (Today is) 
+if n = 1 then number of duplicates is (Today, is) 
+if n = 3 duplicates is 0
+'''
