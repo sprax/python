4e167a6947d86a1c54eb9e45437892540314bf24
sprax <sprax@comcast.net>
2019-08-06 01:13:26 -0400

    reordered algo

--- iq/subseqs.py
+++ iq/subseqs.py
@@ -2,8 +2,8 @@
 r'''
 Find the clustered sequences of indices of characters in a string S that match
 those of a given search string T, in-order but not necessarily contiguously.
-location of BCD in String S. 
-Given for example S = BCXXBXXCXDXBCD and T = BCD, then the result should be [[4,7,9],[11,12,13] 
+location of BCD in String S.
+Given for example S = BCXXBXXCXDXBCD and T = BCD, then the result should be [[4,7,9],[11,12,13]
 Given T = BCBC and String S = BCXXBXCXBCBC, the result should be [[0,1,4,6],[8,9,10,11]]
 '''
 from __future__ import print_function
@@ -18,7 +18,7 @@ import argparse
 def sub_str_idxs(seq_str, sub_str):
     '''
     test sub-sequences (or non-contiguous embedded string indices) stuff
-    Given for example S = BCXXBXXCXDXBCD, then the result should be [[4,7,9],[11,12,13] 
+    Given for example S = BCXXBXXCXDXBCD, then the result should be [[4,7,9],[11,12,13]
     '''
     result = []
     sub_res = []
@@ -26,22 +26,22 @@ def sub_str_idxs(seq_str, sub_str):
     if seq_str and sub_str:
         sub_idx = 0
         nxt_chr = sub_str[sub_idx]
-        print("nxt_chr:", nxt_chr)
+        # print("nxt_chr:", nxt_chr)
         for seq_idx, seq_chr in enumerate(seq_str):
-            print("        idx, chr:", seq_idx, seq_chr)
-            if seq_chr == nxt_chr:
-                sub_res.append(seq_idx)
-                print("sub_res:", sub_res)
-                if len(sub_res) == len_sub:
-                    result.append(sub_res) 
-                    sub_res = []
-                    sub_idx = 0
-                else:
-                    sub_idx += 1
-                    nxt_chr = sub_str[sub_idx]
-                    print("nxt_chr:", nxt_chr)
-            elif seq_chr in sub_str:
-                if seq_chr == sub_str[0]:
+            # print("        idx, chr:", seq_idx, seq_chr)
+            if seq_chr in sub_str:
+                if seq_chr == nxt_chr:
+                    sub_res.append(seq_idx)
+                    # print("sub_res:", sub_res)
+                    if len(sub_res) == len_sub:
+                        result.append(sub_res)
+                        sub_res = []
+                        sub_idx = 0
+                    else:
+                        sub_idx += 1
+                        nxt_chr = sub_str[sub_idx]
+                        # print("nxt_chr:", nxt_chr)
+                elif seq_chr == sub_str[0]:
                     sub_res = [seq_idx]
                     sub_idx = 1
                     nxt_chr = sub_str[sub_idx]
