190b06abedadee77e872e2ee553a131c418c36cd
sprax <sprax@comcast.net>
2019-09-28 08:58:31 -0400

    set_add

--- iq/pair_to_com_anc.py
+++ iq/pair_to_com_anc.py
@@ -15,28 +15,40 @@ from collections import defaultdict
 
 ParentChildPair = namedtuple("ParentChildPair", "parent child")
 
-
-
 '''
 input: list of parent-child ID pairs, and two child IDs.
 output: common ancestors: bool, first, or full set.
 
 '''
+def set_add(set, elt):
+    ''' Add elt to set and return True IFF elt not already in set. '''
+    if elt in set:
+        return False
+    set.add(elt)
+    return True
 
-def have_common_ancestor(pairs, id_a, id_b):
+def have_common_ancestor_ab(pairs, id_a, id_b):
     '''
     returns True if id_a and id_b have a common ancestor
     as found in parent-child pairs
     '''
-    # find all ancestors of id_a
+    # anc_ddict = defaultdict(set) # for all ancestors of any
+    # find all ancestors of id_a, id_b
     anc_set_a = set()
-    anc_dct_a = defaultdict(list)
-    for parent, child in pairs:
-        print("PC", parent, child)
-        if child == id_a:
-            anc_set_a.add(parent)
-    return False
-
+    anc_set_b = set()
+    while True:
+        anc_added = False
+        for parent, child in pairs:
+            print("PC", parent, child)
+            if child == id_a or child in anc_set_a:
+                anc_added = set_add(anc_set_a, parent)
+            if child == id_b or child in anc_set_b:
+                anc_added = set_add(anc_set_b, parent)
+        if not anc_added:
+            break
+    print("anc_set_a:", anc_set_a)
+    print("anc_set_b:", anc_set_b)
+    return anc_set_a.intersection(anc_set_b)
 
 
 
@@ -46,7 +58,7 @@ def test_func_1(func_1, inputs, expect, verbose):
     0 if equal (result == expect),
     1 if not.
     '''
-    result = func_1(inputs)
+    result = func_1(*inputs)
     passed = result == expect
     if verbose > passed:
         print("%s %s:" % (func_1.__name__, "PASS" if passed else "FAIL"))
@@ -57,20 +69,20 @@ def test_func_1(func_1, inputs, expect, verbose):
 
 
 def unit_test(args):
-    ''' test PCP CA functions '''
+    ''' test PCP Common Ancestor functions '''
     verbose = args.verbose
     num_wrong = 0
-    pairs = [ParentChildPair(1, 3),
-             ParentChildPair(3, 5),
-             ParentChildPair(5, 7),
-             ParentChildPair(7, 23),
-             ParentChildPair(2, 4),
-             ParentChildPair(4, 6),
-             ParentChildPair(6, 8),
-             ParentChildPair(8, 23),
+    pairs = [ParentChildPair(3, 1),
+             ParentChildPair(7, 5),
+             ParentChildPair(5, 3),
+             ParentChildPair(23, 7),
+             ParentChildPair(4, 2),
+             ParentChildPair(8, 6),
+             ParentChildPair(6, 4),
+             ParentChildPair(23, 8),
             ]
-    have = have_common_ancestor(pairs, 1, 2)
-    print("have:", have)
+    num_wrong += test_func_1(have_common_ancestor_ab, [pairs, 1, 2], {23}, 1)
+    print("num_wrong:", num_wrong)
     return num_wrong
 
 
@@ -80,7 +92,6 @@ def main():
     parser.add_argument('-verbose', type=int, nargs='?', const=2, default=1,
                         help='verbosity of output (const=2, default: 1)')
     args = parser.parse_args()
-
     unit_test(args)
 
 
