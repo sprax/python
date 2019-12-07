e7809a2826dd46e187251b9640e87564804fcb6d
sprax <sprax@comcast.net>
2019-12-06 23:57:46 -0500

    roman

--- iq/roman.py
+++ iq/roman.py
@@ -7,6 +7,8 @@ Output:
 Expected:
 1994
 '''
+import argparse
+import sys
 
 def romanToIntA(self, s):
     """
@@ -44,15 +46,16 @@ def romanToIntA(self, s):
 
 class Solution(object):
 
-    self.rondo = {
-        'I' : 1,
-        'V' : 5,
-        'X' : 10,
-        'L' : 50,
-        'C' : 100,
-        'D' : 500,
-        'M' : 1000
-    }
+    def __init__(self):
+        self.rondo = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000,
+        }
 
     def romanToInt(self, s):
         """
@@ -62,5 +65,31 @@ class Solution(object):
         z = len(s) - 1
         if z < 0:
             return 0
-        n = rondo.get(s[z])
+        n = self.rondo.get(s[z])
         return n
+
+def main():
+    '''drive tests of Roman number converters'''
+    parser = argparse.ArgumentParser(description="convert Roman number strings to ints")
+    parser.add_argument('-verbose', type=int, nargs='?', const=1, default=1,
+                        help='verbosity of output (default: 1)')
+    args = parser.parse_args()
+    vinfo = sys.version_info
+    v_major = vinfo[0]
+    v_minor = vinfo[1]
+    ver_str = "Python %d.%d" % (v_major, v_minor)
+    print(ver_str)
+
+    test_pairs = {
+        "MCMXCIV": 1994,
+    }
+
+    soln = Solution()
+    for roman in test_pairs:
+        expect = test_pairs[roman]
+        actual = soln.romanToInt(roman)
+        print("Roman {} => {} =?= {}".format(roman, actual, expect))
+
+
+if __name__ == '__main__':
+    main()
