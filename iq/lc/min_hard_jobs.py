94abfefe2352aaccc277a7959f21cfd151dde0da
sprax <sprax@comcast.net>
2020-02-01 16:22:20 -0500

    start simplifying min_cost

--- iq/lc/min_hard_jobs.py
+++ iq/lc/min_hard_jobs.py
@@ -5,30 +5,47 @@ import pdb
 from pdb import set_trace
 
 class Solution:
-    def minDifficulty(self, jobDifficulty, days) -> int:
+    '''
+    1 <= job_cost.length <= 300
+    0 <= job_cost[i] <= 1000
+    1 <= d <= 10
+    '''
 
-        jobs = len(jobDifficulty)
-        # set_trace()
-        if jobs < days:
+    def min_cost(self, job_cost, days) -> int:
+        ''' non-recursive driver '''
+        num_jobs = len(job_cost)
+        if num_jobs < days:
             return -1
-        min_hard = self.minDifficultyRec(0, jobDifficulty, days, jobs, -1)
-        if min_hard >= 0:
-            return min_hard
-        return -1
+        return self.min_cost_rec(0, job_cost, days)
 
-    def minDifficultyRec(self, offset: int, jobDifficulty, days, jobs, best) -> int:
-        if days < 1:
-            return -1
-        min_rest = 0
-        num_jobs = len(jobDifficulty) - offset
-        max_hard = jobDifficulty[offset]
-        for j in range(offset + 1, num_jobs - days + 1):
-            hard = jobDifficulty[j]
-            if max_hard < hard:
+
+    def min_cost_rec(self, offset, job_cost, days) -> int:
+        ''' recursive helper '''
+        assert(days > 0)
+        num_jobs = len(job_cost) - offset
+        assert(num_jobs >= days)
+
+        if num_jobs == days:
+            # set_trace()
+            return sum(job_cost[offset:])
+
+        if days == 1:
+            return max(job_cost[offset:])
+
+        max_hard = job_cost[offset]
+        end = len(job_cost) - days + 1
+        min_hard = 2**32
+        for j in range(offset, end):
+            hard = job_cost[j]
+            if (max_hard < hard):
                 max_hard = hard
-            min_rest = minDifficultyRec(j, jobDifficulty, days - 1, num_jobs - 1, best)
-            min_hard = max_hard + min_rest
-        return max_hard + min_rest
+            # set_trace()
+            min_rest = self.min_cost_rec(j + 1, job_cost, days - 1)
+            min_both = max_hard + min_rest
+            if (min_hard > min_both):
+                min_hard = min_both
+
+        return min_hard
 
 def test_one(expect, func, args):
     actual = func(*args)
@@ -42,9 +59,36 @@ def main():
     sol = Solution()
     num_wrong = 0
 
-    job_difficulty = [1, 2, 3, 4]
-    num_wrong += test_one(-1, sol.minDifficulty, (job_difficulty, 5))
-    num_wrong += test_one(10, sol.minDifficulty, (job_difficulty, 4))
+    job_cost = [11]
+    num_wrong += test_one(11, sol.min_cost, (job_cost, 1))
+    job_cost = [22, 33]
+    num_wrong += test_one(55, sol.min_cost, (job_cost, 2))
+    job_cost = [22, 33, 44]
+    num_wrong += test_one(66, sol.min_cost, (job_cost, 2))
+
+    job_cost = [44, 33, 22]
+    num_wrong += test_one(66, sol.min_cost, (job_cost, 2))
+
+    job_cost = [11, 22, 35, 40]
+    num_wrong += test_one(51, sol.min_cost, (job_cost, 2))
+    num_wrong += test_one(73, sol.min_cost, (job_cost, 3))
+
+    job_cost = [40, 35, 22, 11]
+    num_wrong += test_one(51, sol.min_cost, (job_cost, 2))
+    num_wrong += test_one(73, sol.min_cost, (job_cost, 3))
+
+    job_cost = [57, 45, 33, 22, 10]
+    num_wrong += test_one(57, sol.min_cost, (job_cost, 1))
+    num_wrong += test_one(67, sol.min_cost, (job_cost, 2))
+    num_wrong += test_one(89, sol.min_cost, (job_cost, 3))
+    num_wrong += test_one(122, sol.min_cost, (job_cost, 4))
+
+    job_cost = [1, 2]
+    num_wrong += test_one(3, sol.min_cost, (job_cost, 2))
+
+    job_cost = [1, 2, 3, 4]
+    num_wrong += test_one(-1, sol.min_cost, (job_cost, 5))
+    num_wrong += test_one(10, sol.min_cost, (job_cost, 4))
 
     print("num_wrong: %d" % num_wrong)
 
