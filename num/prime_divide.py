ebc7bde02066939be1db7c55c21d2480fb98b19d
sprax <sprax@comcast.net>
2020-05-29 13:59:12 -0400

    is_prime

--- num/prime_divide.py
+++ num/prime_divide.py
@@ -3,13 +3,23 @@
 from __future__ import print_function
 import math
 
+def is_prime(self, num: int) -> bool:
+    bound = 1 + math.ceil(math.sqrt(num))
+    for fac in range(2, bound):
+        if num % fac == 0:
+            # print("%d equals %d * %d" % (n, fac, n//fac))
+            return False
+    else:   # loop fell through without finding a factor
+        return True
+
+
 print(str(2) + ' is a prime number')
-for n in range(3, 100):
-    bound = 1 + math.ceil(math.sqrt(n))
+for num in range(3, 100):
+    bound = 1 + math.ceil(math.sqrt(num))
     for x in range(2, bound):
-        if n % x == 0:
-            print(str(n) + ' equals ' + str(x) + ' * ' + str(n//x))
+        if num % x == 0:
+            print(str(num) + ' equals ' + str(x) + ' * ' + str(num//x))
             break
     else:
         # loop fell through without finding a factor
-        print(str(n) + ' is a prime number')
+        print(str(num) + ' is a prime number')
