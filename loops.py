3f81ba1bbc18a578851373d5bed03c56556bfbe7
sprax <sprax@comcast.net>
2021-04-27 23:31:21 -0400

    format/style

--- loops.py
+++ loops.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# @file: loops.py
+# @auth: Sprax Lines
+# @date: 2016-06-07 16:33:12 Tue 07 Jun
+
 # loops.py
 for j in range(10):
     print(j, end=',')
@@ -20,6 +25,8 @@ for n in range(2, 10):
 
 
 # fibonacci_generator
+
+
 def fib_generate(n):
     a, b, x = 0, 1, 0
     while x < n:
@@ -44,11 +51,14 @@ def printFibGen(n):    # write Fibonacci series up to n
 
 printFibGen(18)
 
+
 def throw_io_error():
     raise IOError('refusenik user')
 
+
 def constant_factory(value):
     return lambda: value
+
 
 def ask_yes_no(prompt, retries=3, complaint='Yes or no, please!', default_function=constant_factory(False)):
     while True:
@@ -63,11 +73,13 @@ def ask_yes_no(prompt, retries=3, complaint='Yes or no, please!', default_functi
             return default_function()
         print(complaint)
 
+
 def ask_you_got_it():
     if ask_yes_no("You got it? ", 2, "Give it a yes or no.", default_function=throw_io_error):
         print("You got it!")
     else:
         print("You don't got it.")
+
 
 if __name__ == '__main__':
     ask_you_got_it()
