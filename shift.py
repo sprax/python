46ea44744dd2b688de4261b142cc36d9053c9e4a
sprax <sprax@comcast.net>
2021-05-13 23:52:51 -0400

    shift.py

--- shift.py
+++ shift.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
 '''reverse a string'''
+@file: shift.py
+@auth: Sprax Lines
+@date: 2016-06-07 16:33:12 Tue 07 Jun
 
 import sys
+
 
 def shift_string(string):
     '''shift ==> tisfh'''
     # return string[::-2] + string[1:-1:-2]
     return string[::-2] + string[0:-1][::-2]
+
 
 if __name__ == '__main__':
     STRING = sys.argv[1] if len(sys.argv) > 1 else 'Jesus Mary and Joseph'
