82a07d0c297c259b0f9b1fb3b3ecebff0db403cc
sprax <sprax@comcast.net>
2021-06-03 10:11:16 -0400

    cat_nested.py

--- cat_nested.py
+++ cat_nested.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
+@file: cat_nested.py
+@auth: Sprax Lines
+@date: 2016-07-11 00:20:14 Mon 11 Jul
+
 Concatenate all strings in a list or tuple of nested lists and tuples
 as found, left-to-right.  (Thus nested lists/tuples are handled depth-first.)
 Non-string leaf-nodes are converted to strings.
@@ -9,6 +13,7 @@ Usage (to run unit tests): python cat_nested.py
 
 import collections
 import string
+
 
 def is_iterable(vit):
     '''Is vit an iterable?  Or just a scalar value?
@@ -28,8 +33,11 @@ def cat_nested_rec(its):
         return str(its)
     return ans
 
+
 def cat_nested_dfs(its):
-    '''Concatenate all the strings in a nested iterable via depth-first traversal.'''
+    '''
+    Concatenate all the strings in a nested iterable via depth-first traversal.
+    '''
     ans = ''
     stack = [its]
     while stack:
@@ -43,8 +51,12 @@ def cat_nested_dfs(its):
             ans = str(top) + ans
     return ans
 
+
 def test_cat_nested(function, data):
-    '''Test cat_nested* functions on the given nested lists and/or tuples of strings'''
+    '''
+    Test cat_nested* functions on the given nested lists and/or tuples of
+    strings
+    '''
     print("Nested lists/tuples of strings:")
     print("data = ", data)
     ans = function(data)
@@ -54,7 +66,9 @@ def test_cat_nested(function, data):
 
 
 def test_function(function):
-    '''Test one cat_nested* function on nested lists and/or tuples of strings'''
+    '''
+    Test one cat_nested* function on nested lists and/or tuples of strings
+    '''
     print("Testing function:", function.__qualname__)
     print()
     lts = ['abc', ['def', 'ghi'], ['jkl', [['mno'], 'pqr'], 'stu'], [['vwx', 'yz']]]
@@ -67,12 +81,14 @@ def test_function(function):
     ans = test_cat_nested(function, lts)
     assert ans == 'abc123ghi456mno789stuvwxyz'
 
+
 def unit_test():
     '''cat_nested unit test'''
     print("cat_nested unit_test:")
     print(__doc__)
     test_function(cat_nested_rec)
     test_function(cat_nested_dfs)
+
 
 if __name__ == '__main__':
     unit_test()
