37c4c5d3a8fc98d883d35eb5ed7d2d69047362c8
sprax <sprax@comcast.net>
2021-08-03 00:06:06 -0400

    partial.py

--- partial.py
+++ partial.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+# @file: partial.py
+# @auth: Sprax Lines
+# @date: 2017-07-02 01:07:48 Sun 02 Jul
+
 '''bind a function with some args'''
 
+
 def partial(func, *args, **kwargs):
-    '''return partially bound function func with args and kwargs already defined'''
+    '''
+    return partially bound function func with args and kwargs already defined
+    '''
     def wrapped(*args2, **kwargs2):
         return func(*args, *args2, **kwargs, **kwargs2)
     return wrapped
